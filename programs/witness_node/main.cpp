9c8eb7d63bd6db3446ff4643b8592b26dc4b8303
Eric Frias <efrias@syncad.com>
2015-07-16 14:13:42 -0400

    Avoid crashing on error parsing invalid ini file format requried by boost::program_options, temporary workaround for #167

--- programs/witness_node/main.cpp
+++ programs/witness_node/main.cpp
@@ -21,6 +21,7 @@
 #include <graphene/account_history/account_history_plugin.hpp>
 #include <graphene/market_history/market_history_plugin.hpp>
 
+#include <fc/exception/exception.hpp>
 #include <fc/thread/thread.hpp>
 #include <fc/interprocess/signals.hpp>
 #include <fc/log/console_appender.hpp>
@@ -100,9 +101,16 @@ int main(int argc, char** argv) {
          bpo::store(bpo::parse_config_file<char>(config_ini_path.preferred_string().c_str(), cfg_options, true), options);
 
          // try to get logging options from the config file.
-         fc::optional<fc::logging_config> logging_config = load_logging_config_from_ini_file(config_ini_path);
-         if (logging_config)
-            fc::configure_logging(*logging_config);
+         try
+         {
+            fc::optional<fc::logging_config> logging_config = load_logging_config_from_ini_file(config_ini_path);
+            if (logging_config)
+               fc::configure_logging(*logging_config);
+         }
+         catch (const fc::exception&)
+         {
+            wlog("Error parsing logging config from config file ${config}, using default config", ("config", config_ini_path.preferred_string()));
+         }
       }
       else 
       {
@@ -195,76 +203,80 @@ void write_default_logging_config_to_stream(std::ostream& out)
 
 fc::optional<fc::logging_config> load_logging_config_from_ini_file(const fc::path& config_ini_filename)
 {
-   fc::logging_config logging_config;
-   bool found_logging_config = false;
-
-   boost::property_tree::ptree config_ini_tree;
-   boost::property_tree::ini_parser::read_ini(config_ini_filename.preferred_string().c_str(), config_ini_tree);
-   for (const auto& section : config_ini_tree)
+   try
    {
-      const std::string& section_name = section.first;
-      const boost::property_tree::ptree& section_tree = section.second;
+      fc::logging_config logging_config;
+      bool found_logging_config = false;
 
-      const std::string console_appender_section_prefix = "log.console_appender.";
-      const std::string file_appender_section_prefix = "log.file_appender.";
-      const std::string logger_section_prefix = "logger.";
-
-      if (boost::starts_with(section_name, console_appender_section_prefix))
+      boost::property_tree::ptree config_ini_tree;
+      boost::property_tree::ini_parser::read_ini(config_ini_filename.preferred_string().c_str(), config_ini_tree);
+      for (const auto& section : config_ini_tree)
       {
-         std::string console_appender_name = section_name.substr(console_appender_section_prefix.length());
-         std::string stream_name = section_tree.get<std::string>("stream");
+         const std::string& section_name = section.first;
+         const boost::property_tree::ptree& section_tree = section.second;
 
-         // construct a default console appender config here
-         // stdout/stderr will be taken from ini file, everything else hard-coded here
-         fc::console_appender::config console_appender_config;
-         console_appender_config.level_colors.emplace_back(
-            fc::console_appender::level_color(fc::log_level::debug, 
-                                              fc::console_appender::color::green));
-         console_appender_config.level_colors.emplace_back(
-            fc::console_appender::level_color(fc::log_level::warn, 
-                                              fc::console_appender::color::brown));
-         console_appender_config.level_colors.emplace_back(
-            fc::console_appender::level_color(fc::log_level::error, 
-                                              fc::console_appender::color::cyan));
-         console_appender_config.stream = fc::variant(stream_name).as<fc::console_appender::stream::type>();
-         logging_config.appenders.push_back(fc::appender_config(console_appender_name, "console", fc::variant(console_appender_config)));
-         found_logging_config = true;
-      }
-      else if (boost::starts_with(section_name, file_appender_section_prefix))
-      {
-         std::string file_appender_name = section_name.substr(file_appender_section_prefix.length());
-         fc::path file_name = section_tree.get<std::string>("filename");
-         if (file_name.is_relative())
-            file_name = fc::absolute(config_ini_filename).parent_path() / file_name;
-         
+         const std::string console_appender_section_prefix = "log.console_appender.";
+         const std::string file_appender_section_prefix = "log.file_appender.";
+         const std::string logger_section_prefix = "logger.";
 
-         // construct a default file appender config here
-         // filename will be taken from ini file, everything else hard-coded here
-         fc::file_appender::config file_appender_config;
-         file_appender_config.filename = file_name;
-         file_appender_config.flush = true;
-         file_appender_config.rotate = true;
-         file_appender_config.rotation_interval = fc::hours(1);
-         file_appender_config.rotation_limit = fc::days(1);
-         logging_config.appenders.push_back(fc::appender_config(file_appender_name, "file", fc::variant(file_appender_config)));
-         found_logging_config = true;
+         if (boost::starts_with(section_name, console_appender_section_prefix))
+         {
+            std::string console_appender_name = section_name.substr(console_appender_section_prefix.length());
+            std::string stream_name = section_tree.get<std::string>("stream");
+
+            // construct a default console appender config here
+            // stdout/stderr will be taken from ini file, everything else hard-coded here
+            fc::console_appender::config console_appender_config;
+            console_appender_config.level_colors.emplace_back(
+               fc::console_appender::level_color(fc::log_level::debug, 
+                                                 fc::console_appender::color::green));
+            console_appender_config.level_colors.emplace_back(
+               fc::console_appender::level_color(fc::log_level::warn, 
+                                                 fc::console_appender::color::brown));
+            console_appender_config.level_colors.emplace_back(
+               fc::console_appender::level_color(fc::log_level::error, 
+                                                 fc::console_appender::color::cyan));
+            console_appender_config.stream = fc::variant(stream_name).as<fc::console_appender::stream::type>();
+            logging_config.appenders.push_back(fc::appender_config(console_appender_name, "console", fc::variant(console_appender_config)));
+            found_logging_config = true;
+         }
+         else if (boost::starts_with(section_name, file_appender_section_prefix))
+         {
+            std::string file_appender_name = section_name.substr(file_appender_section_prefix.length());
+            fc::path file_name = section_tree.get<std::string>("filename");
+            if (file_name.is_relative())
+               file_name = fc::absolute(config_ini_filename).parent_path() / file_name;
+            
+
+            // construct a default file appender config here
+            // filename will be taken from ini file, everything else hard-coded here
+            fc::file_appender::config file_appender_config;
+            file_appender_config.filename = file_name;
+            file_appender_config.flush = true;
+            file_appender_config.rotate = true;
+            file_appender_config.rotation_interval = fc::hours(1);
+            file_appender_config.rotation_limit = fc::days(1);
+            logging_config.appenders.push_back(fc::appender_config(file_appender_name, "file", fc::variant(file_appender_config)));
+            found_logging_config = true;
+         }
+         else if (boost::starts_with(section_name, logger_section_prefix))
+         {
+            std::string logger_name = section_name.substr(logger_section_prefix.length());
+            std::string level_string = section_tree.get<std::string>("level");
+            std::string appenders_string = section_tree.get<std::string>("appenders");
+            fc::logger_config logger_config(logger_name);
+            logger_config.level = fc::variant(level_string).as<fc::log_level>();
+            boost::split(logger_config.appenders, appenders_string, 
+                         boost::is_any_of(" ,"), 
+                         boost::token_compress_on);
+            logging_config.loggers.push_back(logger_config);
+            found_logging_config = true;
+         }
       }
-      else if (boost::starts_with(section_name, logger_section_prefix))
-      {
-         std::string logger_name = section_name.substr(logger_section_prefix.length());
-         std::string level_string = section_tree.get<std::string>("level");
-         std::string appenders_string = section_tree.get<std::string>("appenders");
-         fc::logger_config logger_config(logger_name);
-         logger_config.level = fc::variant(level_string).as<fc::log_level>();
-         boost::split(logger_config.appenders, appenders_string, 
-                      boost::is_any_of(" ,"), 
-                      boost::token_compress_on);
-         logging_config.loggers.push_back(logger_config);
-         found_logging_config = true;
-      }
+      if (found_logging_config)
+         return logging_config;
+      else
+         return fc::optional<fc::logging_config>();
    }
-   if (found_logging_config)
-      return logging_config;
-   else
-      return fc::optional<fc::logging_config>();
+   FC_RETHROW_EXCEPTIONS(warn, "")
 }
