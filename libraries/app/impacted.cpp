f0502ee2f15bdbcf81203f8859983817c23cd2cf
theoreticalbts <theoreticalbts@users.noreply.github.com>
2015-09-17 15:12:08 -0400

    Implement virtual op for settlement cancel #250

--- libraries/app/impacted.cpp
+++ libraries/app/impacted.cpp
@@ -37,7 +37,10 @@ struct get_impacted_account_visitor
    }
 
    void operator()( const limit_order_create_operation& op ) {}
-   void operator()( const limit_order_cancel_operation& op ) {}
+   void operator()( const limit_order_cancel_operation& op )
+   {
+      _impacted.insert( op.fee_paying_account );
+   }
    void operator()( const call_order_update_operation& op ) {}
    void operator()( const fill_order_operation& op )
    {
@@ -182,6 +185,12 @@ struct get_impacted_account_visitor
       for( const auto& in : op.inputs )
          add_authority_accounts( _impacted, in.owner );
    }
+
+   void operator()( const asset_settle_cancel_operation& op )
+   {
+      _impacted.insert( op.account );
+   }
+
 };
 
 void operation_get_impacted_accounts( const operation& op, flat_set<account_id_type>& result )
