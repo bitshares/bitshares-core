f8bb0b4008ad97aeb3923d4c073b949c113c928a
theoreticalbts <theoreticalbts@users.noreply.github.com>
2015-09-21 10:52:45 -0400

    Fix debug mode build

--- libraries/chain/account_object.cpp
+++ libraries/chain/account_object.cpp
@@ -68,6 +68,8 @@ void account_statistics_object::process_fees(const account_object& a, database& 
          assert( network_cut <= core_fee_total );
 
 #ifndef NDEBUG
+         const auto& props = d.get_global_properties();
+
          share_type reserveed = cut_fee(network_cut, props.parameters.reserve_percent_of_fee);
          share_type accumulated = network_cut - reserveed;
          assert( accumulated + reserveed == network_cut );
