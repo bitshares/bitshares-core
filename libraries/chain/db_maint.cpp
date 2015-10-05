1e1617633c48b3fc27700953ff4d66343b662d0f
Daniel Larimer <dan@bitshares.org>
2015-10-05 08:55:32 -0400

    removing hard fork

--- libraries/chain/db_maint.cpp
+++ libraries/chain/db_maint.cpp
@@ -132,9 +132,6 @@ void database::update_active_witnesses()
    assert( _witness_count_histogram_buffer.size() > 0 );
    share_type stake_target = (_total_voting_stake-_witness_count_histogram_buffer[0]) / 2;
 
-#warning HARDFORK - remove this code after Oct 2 testnet is over.
-   if( head_block_time() < fc::time_point_sec(1443900665) )
-      stake_target = _total_voting_stake/2;
    /// accounts that vote for 0 or 1 witness do not get to express an opinion on
    /// the number of witnesses to have (they abstain and are non-voting accounts)
 
@@ -216,11 +213,6 @@ void database::update_active_committee_members()
 { try {
    assert( _committee_count_histogram_buffer.size() > 0 );
    share_type stake_target = (_total_voting_stake-_witness_count_histogram_buffer[0]) / 2;
-
-#warning HARDFORK - remove this code after Oct 2 testnet is over.
-   if( head_block_time() < fc::time_point_sec(1443900665) )
-      stake_target = _total_voting_stake/2;
-
 
    /// accounts that vote for 0 or 1 witness do not get to express an opinion on
    /// the number of witnesses to have (they abstain and are non-voting accounts)
