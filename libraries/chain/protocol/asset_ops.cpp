52d88c57c980ddbf45f8f2f28a1fe5c7e56765f9
Daniel Larimer <dan@bitshares.org>
2015-10-12 14:17:18 -0400

    prevent assets with BIT prefix

--- libraries/chain/protocol/asset_ops.cpp
+++ libraries/chain/protocol/asset_ops.cpp
@@ -24,6 +24,9 @@ bool is_valid_symbol( const string& symbol )
     if( symbol.size() < GRAPHENE_MIN_ASSET_SYMBOL_LENGTH )
         return false;
 
+    if( symbol.substring(0,3) == "BIT" ) 
+       return false;
+
     if( symbol.size() > GRAPHENE_MAX_ASSET_SYMBOL_LENGTH )
         return false;
 
