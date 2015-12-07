f699240e0a4399860af687f53d73def874a21411
theoreticalbts <theoreticalbts@users.noreply.github.com>
2015-12-07 17:53:03 -0500

    config.hpp:  Bump db version

--- libraries/chain/include/graphene/chain/config.hpp
+++ libraries/chain/include/graphene/chain/config.hpp
@@ -140,7 +140,7 @@
 #define GRAPHENE_RECENTLY_MISSED_COUNT_INCREMENT             4
 #define GRAPHENE_RECENTLY_MISSED_COUNT_DECREMENT             3
 
-#define GRAPHENE_CURRENT_DB_VERSION                          "BTS2.3"
+#define GRAPHENE_CURRENT_DB_VERSION                          "BTS2.4"
 
 #define GRAPHENE_IRREVERSIBLE_THRESHOLD                      (70 * GRAPHENE_1_PERCENT)
 
