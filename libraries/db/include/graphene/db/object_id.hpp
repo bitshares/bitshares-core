c63b624caf96e91d5a655954f034c607b2aeb88a
Daniel Larimer <dan@bitshares.org>
2016-02-15 16:01:13 -0500

    fix build errors and warnings
    This commit was rebased by theoreticalbts and assigned issue #581
    Conflict with 146c0c4d8e24ce20f375b4c978995901464a8dae was resolved, minor improvements.

--- libraries/db/include/graphene/db/object_id.hpp
+++ libraries/db/include/graphene/db/object_id.hpp
@@ -53,36 +53,21 @@ namespace graphene { namespace db {
       bool     is_null()const { return number == 0; }
       explicit operator uint64_t()const { return number; }
 
-      friend bool  operator == ( const object_id_type& a, const object_id_type& b )
-      {
-         return a.number == b.number;
-      }
-
-      friend bool  operator != ( const object_id_type& a, const object_id_type& b )
-      {
-         return a.number != b.number;
-      }
+      friend bool  operator == ( const object_id_type& a, const object_id_type& b ) { return a.number == b.number; }
+      friend bool  operator != ( const object_id_type& a, const object_id_type& b ) { return a.number != b.number; }
+      friend bool  operator < ( const object_id_type& a, const object_id_type& b ) { return a.number < b.number; }
+      friend bool  operator > ( const object_id_type& a, const object_id_type& b ) { return a.number > b.number; }
 
       object_id_type& operator++(int) { ++number; return *this; }
       object_id_type& operator++()    { ++number; return *this; }
 
-      friend object_id_type operator+(const object_id_type& a, int delta ) { 
+      friend object_id_type operator+(const object_id_type& a, int delta ) {
          return object_id_type( a.space(), a.type(), a.instance() + delta );
       }
-      friend object_id_type operator+(const object_id_type& a, int64_t delta ) { 
+      friend object_id_type operator+(const object_id_type& a, int64_t delta ) {
          return object_id_type( a.space(), a.type(), a.instance() + delta );
       }
       friend size_t hash_value( object_id_type v ) { return std::hash<uint64_t>()(v.number); }
-
-      friend bool operator< ( const object_id_type& a, const object_id_type& b )
-      {
-         return a.number < b.number;
-      }
-
-      friend bool operator> ( const object_id_type& a, const object_id_type& b )
-      {
-         return a.number > b.number;
-      }
 
       template< typename T >
       bool is() const
@@ -134,40 +119,16 @@ namespace graphene { namespace db {
       template<typename DB>
       const T& operator()(const DB& db)const { return db.get(*this); }
 
-      friend bool  operator != ( const object_id& a, const object_id& b )
-      {
-         return a.instance != b.instance;
-      }
-      friend bool  operator != ( const object_id_type& a, const object_id& b )
-      {
-         return a != object_id_type(b);
-      }
-      friend bool  operator != ( const object_id& a, const object_id_type& b )
-      {
-         return object_id_type(a) != b;
-      }
+      friend bool  operator == ( const object_id& a, const object_id& b ) { return a.instance == b.instance; }
+      friend bool  operator != ( const object_id& a, const object_id& b ) { return a.instance != b.instance; }
+      friend bool  operator == ( const object_id_type& a, const object_id& b ) { return a == object_id_type(b); }
+      friend bool  operator != ( const object_id_type& a, const object_id& b ) { return a != object_id_type(b); }
+      friend bool  operator == ( const object_id& b, const object_id_type& a ) { return a == object_id_type(b); }
+      friend bool  operator != ( const object_id& b, const object_id_type& a ) { return a != object_id_type(b); }
 
+      friend bool  operator < ( const object_id& a, const object_id& b ) { return a.instance.value < b.instance.value; }
+      friend bool  operator > ( const object_id& a, const object_id& b ) { return a.instance.value > b.instance.value; }
 
-      friend bool  operator == ( const object_id& a, const object_id& b )
-      {
-         return a.instance == b.instance;
-      }
-      friend bool  operator == ( const object_id_type& a, const object_id& b )
-      {
-         return a == object_id_type(b);
-      }
-      friend bool  operator == ( const object_id& b, const object_id_type& a )
-      {
-         return a == object_id_type(b);
-      }
-      friend bool  operator < ( const object_id& a, const object_id& b )
-      {
-         return a.instance.value < b.instance.value;
-      }
-      friend bool  operator > ( const object_id& a, const object_id& b )
-      {
-         return a.instance.value > b.instance.value;
-      }
       friend size_t hash_value( object_id v ) { return std::hash<uint64_t>()(v.instance.value); }
 
       unsigned_int instance;
