bitshares/bitshares-core

commit d3c6cbf4de72a8d8003ffdb178e6ec3206062058

Compile fixes for win32 and out-of-source builds

libraries/chain/include/graphene/chain/database.hpp
@@ -22,7 +22,6 @@
  * THE SOFTWARE.
  */
 #pragma once
-#include <graphene/chain/evaluator.hpp>
 #include <graphene/chain/global_property_object.hpp>
 #include <graphene/chain/node_property_object.hpp>
 #include <graphene/chain/account_object.hpp>
@@ -45,6 +44,8 @@
 namespace graphene { namespace chain {
    using graphene::db::abstract_object;
    using graphene::db::object;
+   class op_evaluator;
+   class transaction_evaluation_state;
 
    struct budget_record;
 

libraries/chain/include/graphene/chain/evaluator.hpp
@@ -25,6 +25,7 @@
 #include <graphene/chain/exceptions.hpp>
 #include <graphene/chain/transaction_evaluation_state.hpp>
 #include <graphene/chain/protocol/operations.hpp>
+#include <graphene/chain/database.hpp>
 
 namespace graphene { namespace chain {
 

libraries/chain/CMakeLists.txt
@@ -1,6 +1,6 @@
 
 add_custom_target( build_hardfork_hpp
-                   COMMAND cat-parts hardfork.d "${CMAKE_CURRENT_BINARY_DIR}/include/graphene/chain/hardfork.hpp" )
+                   COMMAND cat-parts "${CMAKE_CURRENT_SOURCE_DIR}/hardfork.d" "${CMAKE_CURRENT_BINARY_DIR}/include/graphene/chain/hardfork.hpp" )
 set_source_files_properties( "${CMAKE_CURRENT_BINARY_DIR}/include/graphene/chain/hardfork.hpp" PROPERTIES GENERATED TRUE )
 
 add_dependencies( build_hardfork_hpp cat-parts )

libraries/chain/db_block.cpp
@@ -34,6 +34,7 @@
 #include <graphene/chain/witness_object.hpp>
 #include <graphene/chain/protocol/fee_schedule.hpp>
 #include <graphene/chain/exceptions.hpp>
+#include <graphene/chain/evaluator.hpp>
 
 namespace graphene { namespace chain {
 

libraries/chain/protocol/fee_schedule.cpp
@@ -21,6 +21,7 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
+#include <algorithm>
 #include <graphene/chain/protocol/fee_schedule.hpp>
 #include <fc/smart_ref_impl.hpp>
 

programs/build_helpers/cat-parts.cpp
@@ -62,6 +62,7 @@ int main( int argc, char** argv, char** envp )
       }
 
       {
+         boost::filesystem::create_directories(opath.parent_path());
          boost::filesystem::ofstream ofs(opath);
          ofs.write( new_data.c_str(), new_data.length() );
       }

tests/tests/fee_tests.cpp
@@ -32,6 +32,7 @@
 #include <graphene/chain/fba_object.hpp>
 #include <graphene/chain/market_object.hpp>
 #include <graphene/chain/vesting_balance_object.hpp>
+#include <graphene/chain/exceptions.hpp>
 
 #include <boost/test/unit_test.hpp>
 

commit 567a40170e17d0d3a776e3bf313e1d4721146de6

Fix gcc/clang builds broken by d3c6cbf4de72a8d8003ffdb178e6ec3206062058 (breaking circular dependency)

libraries/chain/evaluator.cpp
@@ -118,4 +118,14 @@ database& generic_evaluator::db()const { return trx_state->db(); }
          _fba.accumulated_fba_fees += core_fee_paid;
       } );
    }
+
+   share_type generic_evaluator::calculate_fee_for_operation(const operation& op) const
+   {
+     return db().current_fee_schedule().calculate_fee( op ).amount;
+   }
+   void generic_evaluator::db_adjust_balance(const account_id_type& fee_payer, asset fee_from_account)
+   {
+     db().adjust_balance(fee_payer, fee_from_account);
+   }
+
 } }

libraries/chain/include/graphene/chain/database.hpp
@@ -29,6 +29,7 @@
 #include <graphene/chain/fork_database.hpp>
 #include <graphene/chain/block_database.hpp>
 #include <graphene/chain/genesis_state.hpp>
+#include <graphene/chain/evaluator.hpp>
 
 #include <graphene/db/object_database.hpp>
 #include <graphene/db/object.hpp>

libraries/chain/include/graphene/chain/evaluator.hpp
@@ -25,7 +25,6 @@
 #include <graphene/chain/exceptions.hpp>
 #include <graphene/chain/transaction_evaluation_state.hpp>
 #include <graphene/chain/protocol/operations.hpp>
-#include <graphene/chain/database.hpp>
 
 namespace graphene { namespace chain {
 
@@ -103,6 +102,12 @@ namespace graphene { namespace chain {
        */
       void pay_fba_fee( uint64_t fba_id );
 
+      // the next two functions are helpers that allow template functions declared in this 
+      // header to call db() without including database.hpp, which would
+      // cause a circular dependency
+      share_type calculate_fee_for_operation(const operation& op) const;
+      void db_adjust_balance(const account_id_type& fee_payer, asset fee_from_account);
+
       asset                            fee_from_account;
       share_type                       core_fee_paid;
       const account_object*            fee_paying_account = nullptr;
@@ -144,10 +149,11 @@ namespace graphene { namespace chain {
          prepare_fee(op.fee_payer(), op.fee);
          if( !trx_state->skip_fee_schedule_check )
          {
-            GRAPHENE_ASSERT( core_fee_paid >= db().current_fee_schedule().calculate_fee( op ).amount,
+            share_type required_fee = calculate_fee_for_operation(op);
+            GRAPHENE_ASSERT( core_fee_paid >= required_fee,
                        insufficient_fee,
                        "Insufficient Fee Paid",
-                       ("core_fee_paid",core_fee_paid)("required",db().current_fee_schedule().calculate_fee( op ).amount) );
+                       ("core_fee_paid",core_fee_paid)("required", required_fee) );
          }
 
          return eval->do_evaluate(op);
@@ -163,7 +169,7 @@ namespace graphene { namespace chain {
 
          auto result = eval->do_apply(op);
 
-         db().adjust_balance(op.fee_payer(), -fee_from_account);
+         db_adjust_balance(op.fee_payer(), -fee_from_account);
 
          return result;
       }