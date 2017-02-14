0ecdc90d4df2ebd80a6d4cfa4388f4b8a608c9c1
elmato <matias.romeo@gmail.com>
2017-02-14 21:38:40 +0000

    Add the array of ids (that are being removed) to the removed_objects signal

--- libraries/chain/include/graphene/chain/database.hpp
+++ libraries/chain/include/graphene/chain/database.hpp
@@ -204,7 +204,7 @@ namespace graphene { namespace chain {
          /** this signal is emitted any time an object is removed and contains a
           * pointer to the last value of every object that was removed.
           */
-         fc::signal<void(const vector<const object*>&)>  removed_objects;
+         fc::signal<void(const vector<object_id_type>&, const vector<const object*>&)>  removed_objects;
 
          //////////////////// db_witness_schedule.cpp ////////////////////
 
