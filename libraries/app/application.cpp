112d1b0fe7fb8b892c05bf3c5f5e8475eb521f48
Dark Sun <dacom.dark.sun@gmail.com>
2017-11-29 16:13:06 +0500

    Initialization

--- libraries/app/application.cpp
+++ libraries/app/application.cpp
@@ -158,44 +158,7 @@ namespace detail {
                }
             }
          }
-         else
-         {
-            // https://bitsharestalk.org/index.php/topic,23715.0.html
-            vector<string> seeds = {
-               "104.236.144.84:1777",               // puppies      (USA)
-               "128.199.143.47:2015",               // Harvey       (Singapore)
-               "23.92.53.182:1776",                 // sahkan       (USA)
-               "192.121.166.162:1776",              // sahkan       (UK)
-               "51.15.61.160:1776",                 // lafona       (France)
-               "bts-seed1.abit-more.com:62015",     // abit         (China)
-               "node.blckchnd.com:4243",            // blckchnd     (Germany)
-               "seed.bitsharesdex.com:50696",       // iHashFury    (Europe)
-               "seed.bitsharesnodes.com:1776",      // wackou       (Netherlands)
-               "seed.blocktrades.us:1776",          // BlockTrades  (USA)
-               "seed.cubeconnex.com:1777",          // cube         (USA)
-               "seed.roelandp.nl:1776",             // roelandp     (Canada)
-               "seed04.bts-nodes.net:1776",         // Thom         (Australia)
-               "seed05.bts-nodes.net:1776",	        // Thom         (USA)
-               "seed06.bts-nodes.net:1776",	        // Thom         (USA)
-               "seed07.bts-nodes.net:1776",	        // Thom         (Singapore)
-               "seeds.bitshares.eu:1776"            // pc           (http://seeds.quisquis.de/bitshares.html)
-            };
-            for( const string& endpoint_string : seeds )
-            {
-               try {
-                  std::vector<fc::ip::endpoint> endpoints = resolve_string_to_ip_endpoints(endpoint_string);
-                  for (const fc::ip::endpoint& endpoint : endpoints)
-                  {
-                     ilog("Adding seed node ${endpoint}", ("endpoint", endpoint));
-                     _p2p_network->add_node(endpoint);
-                  }
-               } catch( const fc::exception& e ) {
-                  wlog( "caught exception ${e} while adding seed node ${endpoint}",
-                           ("e", e.to_detail_string())("endpoint", endpoint_string) );
-               }
-            }
-         }
-
+         
          if( _options->count("p2p-endpoint") )
             _p2p_network->listen_on_endpoint(fc::ip::endpoint::from_string(_options->at("p2p-endpoint").as<string>()), true);
          else
--- libraries/chain/protocol/account.cpp
+++ libraries/chain/protocol/account.cpp
@@ -117,7 +117,7 @@ bool is_valid_name( const string& name )
              case 'y': case 'z':
              case '0': case '1': case '2': case '3': case '4': case '5': case '6': case '7':
              case '8': case '9':
-             case '-':
+             case '-': case '.':
                 break;
              default:
           ilog( ".");
