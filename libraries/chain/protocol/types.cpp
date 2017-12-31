567d656122f5d805a5898f840e679df85ac9cdf0
lubos.ilcik <lubos.ilcik@touch4it.com>
2017-12-31 10:39:38 +0100

    replace BTS string literal with corresponding constant

--- libraries/chain/protocol/types.cpp
+++ libraries/chain/protocol/types.cpp
@@ -49,7 +49,7 @@ namespace graphene { namespace chain {
        try
        {
            if( is_valid_v1( base58str ) )
-               prefix = std::string( "BTS" );
+               prefix = std::string( GRAPHENE_ADDRESS_PREFIX );
        }
        catch( ... )
        {
@@ -67,7 +67,7 @@ namespace graphene { namespace chain {
     // TODO: This is temporary for testing
     bool public_key_type::is_valid_v1( const std::string& base58str )
     {
-       std::string prefix( "BTS" );
+       std::string prefix( GRAPHENE_ADDRESS_PREFIX );
        const size_t prefix_len = prefix.size();
        FC_ASSERT( base58str.size() > prefix_len );
        FC_ASSERT( base58str.substr( 0, prefix_len ) ==  prefix , "", ("base58str", base58str) );
