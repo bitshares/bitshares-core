7d75f7145f28618f04f74818d1cf548369cf9c73
Fabian Schuh <Fabian@chainsquad.com>
2018-01-29 14:19:03 +0100

    Reflect new parameter

--- libraries/chain/include/graphene/chain/proposal_object.hpp
+++ libraries/chain/include/graphene/chain/proposal_object.hpp
@@ -101,4 +101,4 @@ typedef generic_index<proposal_object, proposal_multi_index_container> proposal_
 FC_REFLECT_DERIVED( graphene::chain::proposal_object, (graphene::chain::object),
                     (expiration_time)(review_period_time)(proposed_transaction)(required_active_approvals)
                     (available_active_approvals)(required_owner_approvals)(available_owner_approvals)
-                    (available_key_approvals) )
+                    (available_key_approvals)(proposer) )
