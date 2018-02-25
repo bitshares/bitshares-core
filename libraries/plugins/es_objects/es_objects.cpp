2bf302d6a855a9f745ae9d9b759e3a53ff2b3517
Alfredo <oxarbitrage@gmail.com>
2018-02-25 16:12:36 -0300

    add utility common elasticsearch calls

--- libraries/plugins/es_objects/es_objects.cpp
+++ libraries/plugins/es_objects/es_objects.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2017 Cryptonomex, Inc., and contributors.
+ * Copyright (c) 2018 oxarbitrage, and contributors.
  *
  * The MIT License
  *
@@ -24,18 +24,14 @@
 
 #include <graphene/es_objects/es_objects.hpp>
 
-#include <graphene/chain/config.hpp>
-#include <graphene/chain/database.hpp>
-
 #include <fc/smart_ref_impl.hpp>
-#include <fc/thread/thread.hpp>
 
 #include <curl/curl.h>
-#include <boost/algorithm/string/replace.hpp>
 #include <graphene/chain/proposal_object.hpp>
-#include <boost/algorithm/string/join.hpp>
 #include <graphene/chain/balance_object.hpp>
 #include <graphene/chain/market_object.hpp>
+
+#include <graphene/utilities/elasticsearch.hpp>
 
 
 namespace graphene { namespace es_objects {
@@ -69,15 +65,14 @@ class es_objects_plugin_impl
       bool _es_objects_limit_orders = true;
       bool _es_objects_logs = true;
       CURL *curl; // curl handler
-      vector <string> bulk;
+      vector <std::string> bulk;
+      vector<std::string> prepare;
    private:
       void PrepareProposal(const proposal_object* proposal_object);
       void PrepareAccount(const account_object* account_object);
       void PrepareAsset(const asset_object* asset_object);
       void PrepareBalance(const balance_object* balance_object);
       void PrepareLimit(const limit_order_object* limit_object);
-      void SendBulk();
-      void createBulk(std::string type, std::string data, std::string id);
 };
 
 void es_objects_plugin_impl::updateDatabase( const vector<object_id_type>& ids , bool isNew)
@@ -95,7 +90,9 @@ void es_objects_plugin_impl::updateDatabase( const vector<object_id_type>& ids ,
       limit_documents = _es_objects_bulk_replay;
 
    if (curl && bulk.size() >= limit_documents) { // we are in bulk time, ready to add data to elasticsearech
-      SendBulk();
+      if(!graphene::utilities::SendBulk(curl, bulk, _es_objects_elasticsearch_url, _es_objects_logs, "objects_logs"))
+         elog("Error sending data to database");
+      bulk.clear();
    }
 
    for(auto const& value: ids) {
@@ -132,77 +129,6 @@ void es_objects_plugin_impl::updateDatabase( const vector<object_id_type>& ids ,
    }
 }
 
-void es_objects_plugin_impl::createBulk(std::string type, std::string data, std::string id)
-{
-   bulk.push_back("{ \"index\" : { \"_index\" : \"bitshares-"+type+"\", \"_type\" : \"data\", \"_id\" : "+id+" } }");
-   bulk.push_back(data);
-}
-
-void es_objects_plugin_impl::SendBulk()
-{
-   // curl buffers to read
-   std::string readBuffer;
-   std::string readBuffer_logs;
-
-   std::string bulking = "";
-
-   bulking = boost::algorithm::join(bulk, "\n");
-   bulking = bulking + "\n";
-   bulk.clear();
-
-   struct curl_slist *headers = NULL;
-   curl_slist_append(headers, "Content-Type: application/json");
-   std::string url = _es_objects_elasticsearch_url + "_bulk";
-   curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
-   curl_easy_setopt(curl, CURLOPT_POST, true);
-   curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
-   curl_easy_setopt(curl, CURLOPT_POSTFIELDS, bulking.c_str());
-   curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
-   curl_easy_setopt(curl, CURLOPT_WRITEDATA, (void *)&readBuffer);
-   curl_easy_setopt(curl, CURLOPT_USERAGENT, "libcrp/0.1");
-   //curl_easy_setopt(curl, CURLOPT_VERBOSE, true);
-   curl_easy_perform(curl);
-
-   long http_code = 0;
-   curl_easy_getinfo (curl, CURLINFO_RESPONSE_CODE, &http_code);
-   if(http_code == 200) {
-      // all good, do nothing
-   }
-   else if(http_code == 429) {
-      // repeat request?
-   }
-   else {
-      // exit everything ?
-   }
-   if(_es_objects_logs) {
-      auto logs = readBuffer;
-      // do logs
-      std::string url_logs = _es_objects_elasticsearch_url + "objects_logs/data/";
-      curl_easy_setopt(curl, CURLOPT_URL, url_logs.c_str());
-      curl_easy_setopt(curl, CURLOPT_POST, true);
-      curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
-      curl_easy_setopt(curl, CURLOPT_POSTFIELDS, logs.c_str());
-      curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
-      curl_easy_setopt(curl, CURLOPT_WRITEDATA, (void *) &readBuffer_logs);
-      curl_easy_setopt(curl, CURLOPT_USERAGENT, "libcrp/0.1");
-      //curl_easy_setopt(curl, CURLOPT_VERBOSE, true);
-      //ilog("log here curl: ${output}", ("output", readBuffer_logs));
-      curl_easy_perform(curl);
-
-      http_code = 0;
-      curl_easy_getinfo (curl, CURLINFO_RESPONSE_CODE, &http_code);
-      if(http_code == 200) {
-         // all good, do nothing
-      }
-      else if(http_code == 429) {
-         // repeat request?
-      }
-      else {
-         // exit everything ?
-      }
-   }
-}
-
 void es_objects_plugin_impl::PrepareProposal(const proposal_object* proposal_object)
 {
    proposal_struct prop;
@@ -216,8 +142,9 @@ void es_objects_plugin_impl::PrepareProposal(const proposal_object* proposal_obj
    prop.available_key_approvals = fc::json::to_string(proposal_object->available_key_approvals);
 
    std::string data = fc::json::to_string(prop);
-
-   createBulk("proposal", data, fc::json::to_string(proposal_object->id));
+   prepare = graphene::utilities::createBulk("bitshares-proposal", data, fc::json::to_string(proposal_object->id), 0);
+   bulk.insert(bulk.end(), prepare.begin(), prepare.end());
+   prepare.clear();
 }
 
 void es_objects_plugin_impl::PrepareAccount(const account_object* account_object)
@@ -241,8 +168,9 @@ void es_objects_plugin_impl::PrepareAccount(const account_object* account_object
    acct.voting_account = account_object->options.voting_account;
 
    std::string data = fc::json::to_string(acct);
-
-   createBulk("account", data, fc::json::to_string(account_object->id));
+   prepare = graphene::utilities::createBulk("bitshares-account", data, fc::json::to_string(account_object->id), 0);
+   bulk.insert(bulk.end(), prepare.begin(), prepare.end());
+   prepare.clear();
 }
 
 void es_objects_plugin_impl::PrepareAsset(const asset_object* asset_object)
@@ -253,8 +181,9 @@ void es_objects_plugin_impl::PrepareAsset(const asset_object* asset_object)
    asset.issuer = asset_object->issuer;
 
    std::string data = fc::json::to_string(asset);
-
-   createBulk("asset", data, fc::json::to_string(asset_object->id));
+   prepare = graphene::utilities::createBulk("bitshares-asset", data, fc::json::to_string(asset_object->id), 0);
+   bulk.insert(bulk.end(), prepare.begin(), prepare.end());
+   prepare.clear();
 }
 
 void es_objects_plugin_impl::PrepareBalance(const balance_object* balance_object)
@@ -265,7 +194,9 @@ void es_objects_plugin_impl::PrepareBalance(const balance_object* balance_object
    balance.amount = balance_object->balance.amount;
 
    std::string data = fc::json::to_string(balance);
-   createBulk("balance", data, fc::json::to_string(balance_object->owner));
+   prepare = graphene::utilities::createBulk("bitshares-balance", data, fc::json::to_string(balance_object->owner), 0);
+   bulk.insert(bulk.end(), prepare.begin(), prepare.end());
+   prepare.clear();
 }
 
 void es_objects_plugin_impl::PrepareLimit(const limit_order_object* limit_object)
@@ -278,7 +209,9 @@ void es_objects_plugin_impl::PrepareLimit(const limit_order_object* limit_object
    limit.deferred_fee = limit_object->deferred_fee;
 
    std::string data = fc::json::to_string(limit);
-   createBulk("limitorder", data, fc::json::to_string(limit_object->id));
+   prepare = graphene::utilities::createBulk("bitshares-limitorder", data, fc::json::to_string(limit_object->id), 0);
+   bulk.insert(bulk.end(), prepare.begin(), prepare.end());
+   prepare.clear();
 }
 
 es_objects_plugin_impl::~es_objects_plugin_impl()
--- libraries/plugins/es_objects/include/graphene/es_objects/es_objects.hpp
+++ libraries/plugins/es_objects/include/graphene/es_objects/es_objects.hpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2017 Cryptonomex, Inc., and contributors.
+ * Copyright (c) 2018 oxarbitrage, and contributors.
  *
  * The MIT License
  *
@@ -30,12 +30,6 @@ namespace graphene { namespace es_objects {
 
 using namespace chain;
 
-
-static size_t WriteCallback(void *contents, size_t size, size_t nmemb, void *userp)
-{
-   ((std::string*)userp)->append((char*)contents, size * nmemb);
-   return size * nmemb;
-}
 
 namespace detail
 {
