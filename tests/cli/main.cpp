8d27b87293a0c724670e5d277e7e7e6ac4601232
John Jones <jmjatlanta@gmail.com>
2018-03-21 11:12:53 -0500

    Added grouped_orders plugin to tests

--- tests/cli/main.cpp
+++ tests/cli/main.cpp
@@ -129,6 +129,7 @@ std::shared_ptr<graphene::app::application> start_application(fc::temp_directory
    app1->register_plugin<graphene::account_history::account_history_plugin>();
    app1->register_plugin< graphene::market_history::market_history_plugin >();
    app1->register_plugin< graphene::witness_plugin::witness_plugin >();
+   app1->register_plugin< graphene::grouped_orders::grouped_orders_plugin>();
    app1->startup_plugins();
    boost::program_options::variables_map cfg;
    server_port_number = get_available_port();
