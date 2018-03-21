1c4556d16f4188c0d5dbe675d1c1481644ff914d
John Jones <jmjatlanta@gmail.com>
2018-03-21 10:56:47 -0500

    fixed change in api constructor signature

--- tests/cli/main.cpp
+++ tests/cli/main.cpp
@@ -202,7 +202,7 @@ public:
       wallet_data.ws_password = "";
       websocket_connection  = websocket_client.connect( wallet_data.ws_server );
 
-      api_connection = std::make_shared<fc::rpc::websocket_api_connection>(*websocket_connection);
+      api_connection = std::make_shared<fc::rpc::websocket_api_connection>(*websocket_connection, GRAPHENE_MAX_NESTED_OBJECTS);
 
       remote_login_api = api_connection->get_remote_api< graphene::app::login_api >(1);
       BOOST_CHECK(remote_login_api->login( wallet_data.ws_user, wallet_data.ws_password ) );
@@ -213,7 +213,7 @@ public:
 
       wallet_api = fc::api<graphene::wallet::wallet_api>(wallet_api_ptr);
 
-      wallet_cli = std::make_shared<fc::rpc::cli>();
+      wallet_cli = std::make_shared<fc::rpc::cli>(GRAPHENE_MAX_NESTED_OBJECTS);
       for( auto& name_formatter : wallet_api_ptr->get_result_formatters() )
          wallet_cli->format_result( name_formatter.first, name_formatter.second );
 
