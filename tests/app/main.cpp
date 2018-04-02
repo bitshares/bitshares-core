b04041fa815dc87c2523a120ff820b6a6172ecad
John Jones <jmjatlanta@gmail.com>
2018-04-02 14:20:37 -0500

    broke out node and application impls for easier testing

--- tests/app/main.cpp
+++ tests/app/main.cpp
@@ -29,6 +29,9 @@
 #include <graphene/utilities/tempdir.hpp>
 
 #include <graphene/account_history/account_history_plugin.hpp>
+#include <graphene/market_history/market_history_plugin.hpp>
+#include <graphene/witness/witness.hpp>
+#include <graphene/grouped_orders/grouped_orders_plugin.hpp>
 
 #include <fc/thread/thread.hpp>
 #include <fc/smart_ref_impl.hpp>
@@ -38,40 +41,98 @@
 #define BOOST_TEST_MODULE Test Application
 #include <boost/test/included/unit_test.hpp>
 
+
+/////////
+/// @brief forward declaration, using as a hack to generate a genesis.json file
+/// for testing
+/////////
+namespace graphene { namespace app { namespace detail {
+   graphene::chain::genesis_state_type create_example_genesis();
+} } } // graphene::app::detail
+
+/////////
+/// @brief create a genesis_json file
+/// @param directory the directory to place the file "genesis.json"
+/// @returns the full path to the file
+////////
+boost::filesystem::path create_genesis_file(fc::temp_directory& directory) {
+   boost::filesystem::path genesis_path = boost::filesystem::path{directory.path().generic_string()} / "genesis.json";
+   fc::path genesis_out = genesis_path;
+   graphene::chain::genesis_state_type genesis_state = graphene::app::detail::create_example_genesis();
+
+   /* Work In Progress: Place some accounts in the Genesis file so as to pre-make some accounts to play with
+   std::string test_prefix = "test";
+   // helper lambda
+   auto get_test_key = [&]( std::string prefix, uint32_t i ) -> public_key_type
+   {
+      return fc::ecc::private_key::regenerate( fc::sha256::hash( test_prefix + prefix + std::to_string(i) ) ).get_public_key();
+   };
+
+   // create 2 accounts to use
+   for (int i = 1; i <= 2; ++i )
+   {
+      genesis_state_type::initial_account_type dev_account(
+            test_prefix + std::to_string(i),
+            get_test_key("owner-", i),
+            get_test_key("active-", i),
+            false);
+
+      genesis_state.initial_accounts.push_back(dev_account);
+      // give her some coin
+
+   }
+   */
+
+   fc::json::save_to_file(genesis_state, genesis_out);
+   return genesis_path;
+}
+
 using namespace graphene;
 
+/////////////
+/// @brief create a 2 node network
+/////////////
 BOOST_AUTO_TEST_CASE( two_node_network )
 {
    using namespace graphene::chain;
    using namespace graphene::app;
    try {
-      BOOST_TEST_MESSAGE( "Creating temporary files" );
-
-      fc::temp_directory app_dir( graphene::utilities::temp_directory_path() );
-      fc::temp_directory app2_dir( graphene::utilities::temp_directory_path() );
-      fc::temp_file genesis_json;
-
       BOOST_TEST_MESSAGE( "Creating and initializing app1" );
 
+      fc::temp_directory app_dir( graphene::utilities::temp_directory_path() );
+
       graphene::app::application app1;
-      app1.register_plugin<graphene::account_history::account_history_plugin>();
+      app1.register_plugin< graphene::account_history::account_history_plugin>();
+      app1.register_plugin< graphene::market_history::market_history_plugin >();
+      app1.register_plugin< graphene::witness_plugin::witness_plugin >();
+      app1.register_plugin< graphene::grouped_orders::grouped_orders_plugin>();
+      app1.startup_plugins();
       boost::program_options::variables_map cfg;
       cfg.emplace("p2p-endpoint", boost::program_options::variable_value(string("127.0.0.1:3939"), false));
+      cfg.emplace("genesis-json", boost::program_options::variable_value(create_genesis_file(app_dir), false));
+      cfg.emplace("seed-nodes", boost::program_options::variable_value(string("[]"), false));
       app1.initialize(app_dir.path(), cfg);
+      BOOST_TEST_MESSAGE( "Starting app1 and waiting 500 ms" );
+      app1.startup();
+      fc::usleep(fc::milliseconds(500));
 
       BOOST_TEST_MESSAGE( "Creating and initializing app2" );
 
+      fc::temp_directory app2_dir( graphene::utilities::temp_directory_path() );
       graphene::app::application app2;
       app2.register_plugin<account_history::account_history_plugin>();
+      app2.register_plugin< graphene::market_history::market_history_plugin >();
+      app2.register_plugin< graphene::witness_plugin::witness_plugin >();
+      app2.register_plugin< graphene::grouped_orders::grouped_orders_plugin>();
+      app2.startup_plugins();
       auto cfg2 = cfg;
       cfg2.erase("p2p-endpoint");
       cfg2.emplace("p2p-endpoint", boost::program_options::variable_value(string("127.0.0.1:4040"), false));
+      cfg2.emplace("genesis-json", boost::program_options::variable_value(create_genesis_file(app_dir), false));
       cfg2.emplace("seed-node", boost::program_options::variable_value(vector<string>{"127.0.0.1:3939"}, false));
+      cfg2.emplace("seed-nodes", boost::program_options::variable_value(string("[]"), false));
       app2.initialize(app2_dir.path(), cfg2);
 
-      BOOST_TEST_MESSAGE( "Starting app1 and waiting 500 ms" );
-      app1.startup();                                      
-      fc::usleep(fc::milliseconds(500));
       BOOST_TEST_MESSAGE( "Starting app2 and waiting 500 ms" );
       app2.startup();
       fc::usleep(fc::milliseconds(500));
