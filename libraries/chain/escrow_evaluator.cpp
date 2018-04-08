6b867de3242f3014fd1b6e44a015a0f7848110ef
Alfredo <oxarbitrage@gmail.com>
2018-04-08 19:49:46 -0300

    add the operations code

--- libraries/chain/escrow_evaluator.cpp
+++ libraries/chain/escrow_evaluator.cpp
@@ -23,12 +23,9 @@
  */
 #include <graphene/chain/database.hpp>
 #include <graphene/chain/escrow_evaluator.hpp>
-
-//#include <graphene/chain/account_object.hpp>
-//#include <graphene/chain/vesting_balance_object.hpp>
 #include <graphene/chain/escrow_object.hpp>
+//#include <graphene/chain/db_balance.hpp>
 
-//#include <graphene/chain/protocol/vote.hpp>
 
 namespace graphene { namespace chain {
 
@@ -41,6 +38,32 @@ namespace graphene { namespace chain {
       object_id_type escrow_transfer_evaluator::do_apply(const escrow_transfer_operation& o)
       {
 
+         try {
+            //FC_ASSERT( db().has_hardfork( STEEMIT_HARDFORK_0_9 ) ); /// TODO: remove this after HF9
+
+            //const auto& from_account = o.from(db());
+            //const auto& to_account = o.to(db());
+            //const auto& agent_account = o.agent(db());
+
+            FC_ASSERT( db().get_balance( o.from, o.amount.asset_id ) >= (o.amount + o.fee) );
+
+            if( o.fee.amount > 0 ) {
+               db().adjust_balance( o.from, -o.fee );
+               db().adjust_balance( o.agent, o.fee );
+            }
+
+            db().adjust_balance( o.from, -o.amount );
+
+            db().create<escrow_object>([&]( escrow_object& esc ) {
+               esc.escrow_id  = o.escrow_id;
+               esc.from       = o.from;
+               esc.to         = o.to;
+               esc.agent      = o.agent;
+               esc.balance    = o.amount;
+               esc.expiration = o.expiration;
+            });
+
+         } FC_CAPTURE_AND_RETHROW( (o) )
       }
 
 
@@ -53,6 +76,18 @@ namespace graphene { namespace chain {
       object_id_type escrow_dispute_evaluator::do_apply(const escrow_dispute_operation& o)
       {
 
+         try {
+            //FC_ASSERT( db().has_hardfork( STEEMIT_HARDFORK_0_9 ) ); /// TODO: remove this after HF9
+            //const auto& from_account = db().get_account(o.from);
+
+            const auto& e = db().get_escrow( o.from, o.escrow_id );
+            FC_ASSERT( !e.disputed );
+            FC_ASSERT( e.to == o.to );
+
+            db().modify( e, [&]( escrow_object& esc ){
+               esc.disputed = true;
+            });
+         } FC_CAPTURE_AND_RETHROW( (o) )
       }
 
 
@@ -65,6 +100,32 @@ namespace graphene { namespace chain {
       object_id_type escrow_release_evaluator::do_apply(const escrow_release_operation& o)
       {
 
+         try {
+            //FC_ASSERT( db().has_hardfork( STEEMIT_HARDFORK_0_9 ) ); /// TODO: remove this after HF9
+
+            //const auto& from_account = db().get_account(o.from);
+            //const auto& to_account = db().get_account(o.to);
+            //const auto& who_account = db().get_account(o.who);
+
+            const auto& e = db().get_escrow( o.from, o.escrow_id );
+            FC_ASSERT( e.balance >= o.amount && e.balance.asset_id == o.amount.asset_id );
+            /// TODO assert o.amount > 0
+
+            if( o.who == e.from )    FC_ASSERT( o.to == e.to );
+            else if( o.who == e.to ) FC_ASSERT( o.to == e.from );
+            else {
+               FC_ASSERT( e.disputed && o.who == e.agent );
+            }
+
+            db().adjust_balance( o.to, o.amount );
+            if( e.balance == o.amount )
+               db().remove( e );
+            else {
+               db().modify( e, [&]( escrow_object& esc ) {
+                  esc.balance -= o.amount;
+               });
+            }
+         } FC_CAPTURE_AND_RETHROW( (o) )
       }
 
 
