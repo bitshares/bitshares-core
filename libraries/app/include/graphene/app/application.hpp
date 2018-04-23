ac46990b19ca346515f6e32b09ab1a3bc257ba9e
Abit <abitmore@users.noreply.github.com>
2018-04-23 11:52:15 +0200

    Added message to FC_ASSERT in application.hpp

--- libraries/app/include/graphene/app/application.hpp
+++ libraries/app/include/graphene/app/application.hpp
@@ -82,7 +82,7 @@ namespace graphene { namespace app {
          {
             std::shared_ptr<abstract_plugin> abs_plugin = get_plugin( name );
             std::shared_ptr<PluginType> result = std::dynamic_pointer_cast<PluginType>( abs_plugin );
-            FC_ASSERT( result != std::shared_ptr<PluginType>() );
+            FC_ASSERT( result != std::shared_ptr<PluginType>(), "Unable to load plugin '${p}'", ("p",name) );
             return result;
          }
 
