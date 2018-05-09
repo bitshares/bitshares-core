1e0c369e7548b69e1ecc6cb061f6a69e5afe89d0
cifer <mantianyu@gmail.com>
2018-05-10 02:46:31 +0800

    update

--- tests/tests/database_api_tests.cpp
+++ tests/tests/database_api_tests.cpp
@@ -692,4 +692,85 @@ BOOST_AUTO_TEST_CASE( lookup_vote_ids )
 
 } FC_LOG_AND_RETHROW() }
 
+BOOST_AUTO_TEST_CASE(get_account_limit_orders)
+{ try {
+
+   ACTORS((seller));
+
+   const auto& bitcny = create_bitasset("CNY");
+   const auto& core   = asset_id_type()(db);
+
+   int64_t init_balance(10000000);
+   transfer(committee_account, seller_id, asset(init_balance));
+   BOOST_CHECK_EQUAL( 10000000, get_balance(seller, core) );
+
+   /// Create 250 versatile orders
+   for (int i = 0 ; i < 50 ; ++i) {
+     BOOST_CHECK(create_sell_order(seller, core.amount(100), bitcny.amount(250)));
+   }
+
+   for (int i = 1 ; i < 101 ; ++i) {
+     BOOST_CHECK(create_sell_order(seller, core.amount(100), bitcny.amount(250 + i)));
+     BOOST_CHECK(create_sell_order(seller, core.amount(100), bitcny.amount(250 - i)));
+   }
+
+   graphene::app::database_api db_api(db);
+   std::vector<limit_order_object> results;
+   limit_order_object o;
+
+   // query with no constraint, expected:
+   // 1. up to 101 orders returned
+   // 2. orders were sorted by price desendingly
+   results = db_api.get_account_limit_orders(seller.name, "BTS", "CNY");
+   BOOST_CHECK(results.size() == 101);
+   for (int i = 0 ; i < results.size() - 1 ; ++i) {
+     BOOST_CHECK(results[i].sell_price >= results[i+1].sell_price);
+   }
+   results.clear();
+
+   // query with specified limit, expected:
+   // 1. up to specified amount of orders returned
+   // 2. orders were sorted by price desendingly
+   results = db_api.get_account_limit_orders(seller.name, "BTS", "CNY", 50);
+   results = db_api.get_account_limit_orders(seller.name, "BTS", "CNY", 50);
+   BOOST_CHECK(results.size() == 50);
+   for (int i = 0 ; i < results.size() - 1 ; ++i) {
+     BOOST_CHECK(results[i].sell_price >= results[i+1].sell_price);
+   }
+
+   o = results.back();
+   results.clear();
+
+   // query with specified order id and limit, expected:
+   // same as before, but also the first order's id equal to specified
+   results = db_api.get_account_limit_orders(seller.name, "BTS", "CNY", 100,
+       limit_order_id_type(o.id));
+   BOOST_CHECK(results.size() == 100);
+   BOOST_CHECK(results.front().id == o.id);
+   for (int i = 0 ; i < results.size() - 1 ; ++i) {
+     BOOST_CHECK(results[i].sell_price >= results[i+1].sell_price);
+   }
+
+   o = results.back();
+   results.clear();
+
+   // query with specified price and an not exists order id, expected:
+   // 1. the canceled order should not exists in returned orders and first order's
+   //    id should greater than specified
+   // 2. returned orders sorted by price desendingly
+   // 3. the first order's sell price equal to specified
+   cancel_limit_order(o); // NOTE 1: this canceled order was in scope of the
+                          // first created 50 orders, so with price 2.5 BTS/CNY
+   results = db_api.get_account_limit_orders(seller.name, "BTS", "CNY", 101,
+       limit_order_id_type(o.id), o.sell_price);
+   BOOST_CHECK(results.size() <= 101);
+   BOOST_CHECK(results.front().id > o.id);
+   // NOTE 2: because of NOTE 1, here should be equal
+   BOOST_CHECK(results.front().sell_price == o.sell_price);
+   for (int i = 0 ; i < results.size() - 1 ; ++i) {
+     BOOST_CHECK(results[i].sell_price >= results[i+1].sell_price);
+   }
+
+} FC_LOG_AND_RETHROW() }
+
 BOOST_AUTO_TEST_SUITE_END()
