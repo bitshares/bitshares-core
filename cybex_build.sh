179b6c01c265c5635bc4e649ec3c2ba98ec1baa6
hilbertw <hilbertw@cybex.io>
2018-05-22 09:29:36 +0800

    add build number to install dir

--- cybex_build.sh
+++ cybex_build.sh
@@ -8,13 +8,25 @@
 ###########################################################
 
 
+if [ $# -gt 0 ] && [ "$1" == "prod" ];
+then
+   source build_number
+   build=$(( ${BUILD} +1 ))
+
+   install_tag=$(date "+%Y%m%d%H%M%S")."${build}"
+   echo "BUILD=${build}">build_number
+else
+   install_tag=
+fi
+
+
 VERSION=1.2
 ULIMIT=$( ulimit -u )
 WORK_DIR=$PWD
 BUILD_DIR=${WORK_DIR}/build
 ARCH=$( uname -s )
 
-#cd ${WORK_DIR}
+
 if [ ! -d ${BUILD_DIR} ];
 then
     mkdir -p ${BUILD_DIR}
@@ -23,19 +35,17 @@ fi
 cd ${BUILD_DIR}
 
 
-#cmake -DCMAKE_BUILD_TYPE=Release   ..
-cmake -G"Unix Makefiles"  -DOPENSSL_ROOT_DIR=/usr/local/Cellar/openssl/1.0.2o_1  -DBOOST_ROOT=/usr/local/Cellar/boost@1.57/1.57.0/ .. -B.
-
-
 
 if [ "${ARCH}" == "Darwin" ];
     then
+        cmake -G"Unix Makefiles"  -DCMAKE_INSTALL_PREFIX="$PWD/install-${install_tag}" -DGRAPHENE_EGENESIS_JSON=genesis.json -DOPENSSL_ROOT_DIR=/usr/local/Cellar/openssl/1.0.2o_1  -DBOOST_ROOT=/usr/local/Cellar/boost@1.57/1.57.0/ .. -B.
         N_CPU=$(sysctl -i machdep.cpu.thread_count | awk '{print $2}')
     else
+        cmake -G"Unix Makefiles"  -DCMAKE_INSTALL_PREFIX="$PWD/install-${install_tag}" -DGRAPHENE_EGENESIS_JSON=genesis.json .. -B.
         N_CPU=$(cat /proc/cpuinfo | grep -c  '^processor')
 
 fi
 
-make -j${N_CPU} all
+make -j${N_CPU} all install
 
-
+cd ${WORK_DIR}
