a4fabcaae59d84b5960166be7d53a68e4b5f7104
John Jones <jmjatlanta@gmail.com>
2018-05-25 07:44:09 -0500

    changed from const asset_id_type& to asset_id_type

--- libraries/chain/asset_evaluator.cpp
+++ libraries/chain/asset_evaluator.cpp
@@ -498,7 +498,7 @@ void_result asset_update_bitasset_evaluator::do_evaluate(const asset_update_bita
          // Check if the new backing asset is itself backed by something. It must be CORE or a UIA
          if ( new_backing_asset.is_market_issued() )
          {
-            const asset_id_type& backing_backing_asset_id = new_backing_asset.bitasset_data(d).options.short_backing_asset;
+            asset_id_type backing_backing_asset_id = new_backing_asset.bitasset_data(d).options.short_backing_asset;
             FC_ASSERT( (backing_backing_asset_id == asset_id_type() || !backing_backing_asset_id(d).is_market_issued()),
                   "A BitAsset cannot be backed by a BitAsset that itself is backed by a BitAsset.");
          }
@@ -570,7 +570,7 @@ void_result asset_update_bitasset_evaluator::do_evaluate(const asset_update_bita
          // Check if the new backing asset is itself backed by something. It must be CORE or a UIA
          if ( new_backing_asset.is_market_issued() )
          {
-            const asset_id_type& backing_backing_asset_id = new_backing_asset.bitasset_data(d).options.short_backing_asset;
+            asset_id_type backing_backing_asset_id = new_backing_asset.bitasset_data(d).options.short_backing_asset;
             if ( backing_backing_asset_id != asset_id_type() && backing_backing_asset_id(d).is_market_issued() )
             {
                wlog( "before hf-922-931, a BitAsset cannot be backed by a BitAsset that itself "
