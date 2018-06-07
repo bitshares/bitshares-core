8652bd3b45e3e9e1ea1cc5c0ae7ac13d2bc75e88
cifer <mantianyu@gmail.com>
2018-06-07 14:46:25 +0800

    rough implementation of multi-sig transaction

--- libraries/wallet/include/graphene/wallet/wallet.hpp
+++ libraries/wallet/include/graphene/wallet/wallet.hpp
@@ -252,8 +252,15 @@ struct vesting_balance_object_with_info : public vesting_balance_object
    fc::time_point_sec allowed_withdraw_time;
 };
 
-namespace detail {
-class wallet_api_impl;
+struct multisig_trx_data
+{
+   signed_transaction tx;
+   set<public_key_type> required_keys;
+};
+
+namespace detail
+{
+   class wallet_api_impl;
 }
 
 /***
@@ -1599,6 +1606,13 @@ class wallet_api
          
       order_book get_order_book( const string& base, const string& quote, unsigned limit = 50);
 
+      bool multisig_mode(string on_or_off, string tx_filename = "");
+      multisig_trx_data
+      multisig_import_transaction(string tx_filename);
+      multisig_trx_data
+      multisig_sign_transaction(const vector<string>& wif_keys,
+                                bool broadcast = false);
+
       void dbg_make_uia(string creator, string symbol);
       void dbg_make_mia(string creator, string symbol);
       void dbg_push_blocks( std::string src_filename, uint32_t count );
@@ -1693,6 +1707,9 @@ FC_REFLECT(graphene::wallet::operation_detail_ex,
 
 FC_REFLECT( graphene::wallet::account_history_operation_detail,
         (total_count)(result_count)(details))
+
+FC_REFLECT( graphene::wallet::multisig_trx_data,
+        (tx)(required_keys))
 
 FC_API( graphene::wallet::wallet_api,
         (help)
@@ -1815,4 +1832,7 @@ FC_API( graphene::wallet::wallet_api,
         (blind_history)
         (receive_blind_transfer)
         (get_order_book)
+        (multisig_mode)
+        (multisig_import_transaction)
+        (multisig_sign_transaction)
       )
