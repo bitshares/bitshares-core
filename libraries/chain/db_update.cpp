4cb788eb917cee3c389f90c88f19bf2bba69137e
Peter Conrad <conrad@quisquis.de>
2018-06-27 21:10:31 +0200

    Fixed indentation

--- libraries/chain/db_update.cpp
+++ libraries/chain/db_update.cpp
@@ -49,7 +49,7 @@ void database::update_global_dynamic_data( const signed_block& b, const uint32_t
    modify( _dgp, [&b,this,missed_blocks]( dynamic_global_property_object& dgp ){
       if( BOOST_UNLIKELY( b.block_num() == 1 ) )
          dgp.recently_missed_count = 0;
-         else if( _checkpoints.size() && _checkpoints.rbegin()->first >= b.block_num() )
+      else if( _checkpoints.size() && _checkpoints.rbegin()->first >= b.block_num() )
          dgp.recently_missed_count = 0;
       else if( missed_blocks )
          dgp.recently_missed_count += GRAPHENE_RECENTLY_MISSED_COUNT_INCREMENT*missed_blocks;
