4d403989062c58329c678ee11c89bd460bcf522a
abitmore <abitmore@users.noreply.github.com>
2018-07-26 05:49:45 -0400

    Added FC_ASSERT for dynamic data id of core asset

--- libraries/chain/db_init.cpp
+++ libraries/chain/db_init.cpp
@@ -373,6 +373,7 @@ void database::init_genesis(const genesis_state_type& genesis_state)
          a.options.core_exchange_rate.quote.asset_id = asset_id_type(0);
          a.dynamic_asset_data_id = dyn_asset.id;
       });
+   FC_ASSERT( dyn_asset.id == asset_dynamic_data_id_type() );
    FC_ASSERT( asset_id_type(core_asset.id) == asset().asset_id );
    FC_ASSERT( get_balance(account_id_type(), asset_id_type()) == asset(dyn_asset.current_supply) );
    _p_core_asset_obj = &core_asset;
