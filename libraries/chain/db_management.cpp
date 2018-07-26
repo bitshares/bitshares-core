696ed6a534a8b85f478b71985cf179b7791aefcd
abitmore <abitmore@users.noreply.github.com>
2018-07-26 12:53:55 -0400

    Added header inclusions in db_management.cpp

--- libraries/chain/db_management.cpp
+++ libraries/chain/db_management.cpp
@@ -24,6 +24,9 @@
 
 #include <graphene/chain/database.hpp>
 
+#include <graphene/chain/chain_property_object.hpp>
+#include <graphene/chain/witness_schedule_object.hpp>
+#include <graphene/chain/special_authority_object.hpp>
 #include <graphene/chain/operation_history_object.hpp>
 #include <graphene/chain/protocol/fee_schedule.hpp>
 
