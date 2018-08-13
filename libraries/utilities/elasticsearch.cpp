47208c60217af504c87a40d626b5f6f55f2f73e3
Alfredo <oxarbitrage@gmail.com>
2018-08-13 11:07:20 -0300

    change createBulk to accept move

--- libraries/utilities/elasticsearch.cpp
+++ libraries/utilities/elasticsearch.cpp
@@ -118,7 +118,7 @@ bool handleBulkResponse(long http_code, const std::string& CurlReadBuffer)
    return true;
 }
 
-const std::vector<std::string> createBulk(const fc::mutable_variant_object& bulk_header, const std::string& data)
+const std::vector<std::string> createBulk(const fc::mutable_variant_object& bulk_header, const std::string&& data)
 {
    std::vector<std::string> bulk;
    fc::mutable_variant_object final_bulk_header;
