37717054e676dff62ff0c97dc07f4057f7fbb810
John Jones <jmjatlanta@gmail.com>
2018-08-31 10:49:21 -0500

    Cleaned up test a bit

--- tests/app/system_test.cpp
+++ tests/app/system_test.cpp
@@ -106,44 +106,6 @@ int get_available_port()
    return ntohs(sin.sin_port);
 }
 
-///////////
-/// Send a block to the db
-/// @param app the application
-/// @returns true on success
-///////////
-bool generate_block(std::shared_ptr<graphene::app::application> app) {
-   try {
-      fc::ecc::private_key committee_key = fc::ecc::private_key::regenerate(fc::sha256::hash(string("nathan")));
-      auto db = app->chain_database();
-      auto block_1 = db->generate_block( db->get_slot_time(1),
-                                         db->get_scheduled_witness(1),
-                                         committee_key,
-                                         database::skip_nothing );
-      return true;
-   } catch (exception &e) {
-      return false;
-   }
-}
-
-std::shared_ptr<graphene::app::application> start_application(int p2p_port, int rpc_port, 
-      const fc::temp_directory& app_dir, std::string seed_nodes = "[]")
-{
-   std::shared_ptr<graphene::app::application> app = std::make_shared<graphene::app::application>();
-   app->register_plugin< graphene::account_history::account_history_plugin>();
-   app->register_plugin< graphene::market_history::market_history_plugin >();
-   app->register_plugin< graphene::witness_plugin::witness_plugin >();
-   app->register_plugin< graphene::grouped_orders::grouped_orders_plugin>();
-   app->startup_plugins();
-   boost::program_options::variables_map cfg;
-   cfg.emplace("rpc-endpoint", boost::program_options::variable_value("127.0.0.1:" + std::to_string(rpc_port), false));
-   cfg.emplace("p2p-endpoint", boost::program_options::variable_value("127.0.0.1:" + std::to_string(p2p_port), false));
-   cfg.emplace("genesis-json", boost::program_options::variable_value(create_genesis_file(app_dir), false));
-   cfg.emplace("seed-nodes", boost::program_options::variable_value(seed_nodes, false));
-   app->initialize(app_dir.path(), cfg);
-   app->startup();
-   fc::usleep(fc::milliseconds(500));
-   return app;
-}
 
 /*****
  * Makes a client connecting to a node easer
@@ -175,7 +137,7 @@ public:
          wallet_cli->format_result( name_formatter.first, name_formatter.second );
 
       boost::signals2::scoped_connection closed_connection(websocket_connection->closed.connect([=]{
-         std::cerr << "Server has disconnected us.\n";
+         std::cerr << "Client: Server has disconnected us.\n";
          wallet_cli->stop();
       }));
       (void)(closed_connection);
@@ -327,14 +289,14 @@ private:
    }
    void request_block()
    {
-      std::cout << "RequestBlock\n";
+      std::cout << "Client: RequestBlock\n";
    }
    void disconnect_reconnect()
    {
       connection->disconnect();
       is_connected = false;
       connect();
-      std::cout << "Disconnected/reconnected\n";
+      std::cout << "Client: Disconnected/reconnected\n";
    }
    void transfer()
    {
@@ -350,26 +312,101 @@ private:
          std::shared_ptr<client> my_friend = clients->at(my_friend_no);
          // give them some CORE
          connection->wallet_api_ptr->transfer(name, my_friend->name, "1000", "BTS", "", true);
-         std::cout << "Transfer complete\n";
+         std::cout << "client: Transfer complete\n";
       } catch (fc::exception &ex) 
       {
-         std::cerr << "Caught exception attempting to transfer. Error was: " + ex.to_detail_string(fc::log_level(fc::log_level::all)) + "\n";
+         std::cerr << "Client: Caught exception attempting to transfer. Error was: " + ex.to_detail_string(fc::log_level(fc::log_level::all)) + "\n";
       } catch (std::exception &ex)
       {
-        std::cerr << "Caught std exception: " << ex.what() << "\n"; 
+        std::cerr << "Client: Caught std exception attempting to tranfer: " << ex.what() << "\n"; 
       } catch (...)
       {
-        std::cerr << "Caught unknown exception.\n";
+        std::cerr << "Client:Caught unknown exception attempting to transfer.\n";
       }
    }
 };
 
-struct application_server
+class application_server
 {
-   std::shared_ptr<fc::temp_directory> app_dir;
-   unsigned int p2p_port;
-   unsigned int rpc_port;
-   std::shared_ptr<graphene::app::application> app;
+   public:
+      std::shared_ptr<fc::temp_directory> app_dir;
+      unsigned int p2p_port;
+      unsigned int rpc_port;
+      std::shared_ptr<graphene::app::application> app;
+      std::string seed_nodes;
+      volatile bool disconnect_loop_shutdown = true;
+   private:
+      void restart()
+      {
+         app->shutdown();
+         std::this_thread::sleep_for(std::chrono::seconds(3));
+         start_application();
+      }
+      void shutdown()
+      {
+         disconnect_loop_shutdown = true;
+         app->shutdown();
+      }
+   public:
+      void start_disconnect_loop(std::chrono::milliseconds disconnect, std::chrono::microseconds delta)
+      {
+         // calculate a somewhat random time
+         const int64_t rnd = rand() % delta.count();
+         int plus_minus = rand() % 1;
+         std::chrono::nanoseconds disconnect_time = disconnect;
+         if (plus_minus)
+            disconnect_time += std::chrono::nanoseconds(rnd);
+         else
+            disconnect_time -= std::chrono::nanoseconds(rnd);
+         disconnect_loop_shutdown = false;
+         // find a way to tell the application to disconnect from all(?) nodes
+         // maybe just kill it and restart it?
+         std::thread( [disconnect_time, this]() {
+            while( !disconnect_loop_shutdown)
+            {
+               std::this_thread::sleep_for(disconnect_time);
+               std::cout << "Server: Attempting to disconnect from all other nodes and reconnect\n";
+               restart();
+            }
+         }).detach();
+      }
+      std::shared_ptr<graphene::app::application> start_application()
+      {
+         app = std::make_shared<graphene::app::application>();
+         app->register_plugin< graphene::account_history::account_history_plugin>();
+         app->register_plugin< graphene::market_history::market_history_plugin >();
+         app->register_plugin< graphene::witness_plugin::witness_plugin >();
+         app->register_plugin< graphene::grouped_orders::grouped_orders_plugin>();
+         app->startup_plugins();
+         boost::program_options::variables_map cfg;
+         cfg.emplace("rpc-endpoint", boost::program_options::variable_value("127.0.0.1:" + std::to_string(rpc_port), false));
+         cfg.emplace("p2p-endpoint", boost::program_options::variable_value("127.0.0.1:" + std::to_string(p2p_port), false));
+         cfg.emplace("genesis-json", boost::program_options::variable_value(create_genesis_file(*app_dir), false));
+         cfg.emplace("seed-nodes", boost::program_options::variable_value(seed_nodes, false));
+         app->initialize((*app_dir).path(), cfg);
+         app->startup();
+         fc::usleep(fc::milliseconds(500));
+         return app;
+      }
+
+      ///////////
+      /// Send a block to the db
+      /// @param app the application
+      /// @returns true on success
+      ///////////
+      bool generate_block() {
+         try {
+            fc::ecc::private_key committee_key = fc::ecc::private_key::regenerate(fc::sha256::hash(string("nathan")));
+            auto db = app->chain_database();
+            auto block_1 = db->generate_block( db->get_slot_time(1),
+                                             db->get_scheduled_witness(1),
+                                             committee_key,
+                                             database::skip_nothing );
+            return true;
+         } catch (exception &e) {
+            return false;
+         }
+      }
 };
 
 std::string calculate_seed_nodes(const std::vector<std::shared_ptr<application_server>>& servers)
@@ -386,31 +423,43 @@ std::string calculate_seed_nodes(const std::vector<std::shared_ptr<application_s
 }
 
 /****
- * Create a node and a few clients that do random things
+ * Create some nodes and clients that do random things
  */
-int main(int argc, char** argp)
+int main(int argc, char** argv)
 {
-   std::vector<std::shared_ptr<application_server>> servers;
+   unsigned int num_clients = 2;
+   if (argc > 1)
+      num_clients = atoi(argv[1]);
+   unsigned int num_servers = 2;
+   if (argc > 2)
+      num_clients = atoi(argv[2]);
+   std::chrono::seconds client_message_loop_speed(3);
+   std::chrono::seconds server_disconnect_speed(20);
+   std::chrono::milliseconds server_disconnect_plus_minus(1);
 
-   unsigned int num_clients = 10;
-   unsigned int num_servers = 1;
-   std::string seed_nodes = "[]";
+   std::vector<std::shared_ptr<application_server>> servers;
 
    try
    {
+      // set up servers
+      std::string seed_nodes = "[]";
       for(unsigned int i = 0; i < num_servers; ++i)
       {
          std::shared_ptr<application_server> current_server = std::make_shared<application_server>();
          current_server->p2p_port = get_available_port();
          current_server->rpc_port = get_available_port();
          current_server->app_dir = std::make_shared<fc::temp_directory>( graphene::utilities::temp_directory_path() );
-         current_server->app = start_application(current_server->p2p_port, current_server->rpc_port, *current_server->app_dir, seed_nodes);
+         current_server->seed_nodes = seed_nodes;
+         current_server->start_application();
          servers.push_back( current_server );
          seed_nodes = calculate_seed_nodes(servers);
+         if (i != 0)
+            current_server->start_disconnect_loop(server_disconnect_speed, server_disconnect_plus_minus);
       }
+
       std::shared_ptr<application_server> main_server = servers.at(0);
-      // have the nathan account so we can give new users some CORE
       client_connection con(main_server->app, main_server->app_dir->path().generic_string(), main_server->rpc_port, "nathan.json");
+      // have the nathan account give new users some CORE
       con.wallet_api_ptr->set_password("supersecret");
       con.wallet_api_ptr->unlock("supersecret");
       std::vector<std::string> nathan_keys{"5KQwrPbwdL6PhXujxW37FSSQZ1JiwsST4cqQzDeyXtP79zkvFD3"};
@@ -427,7 +476,8 @@ int main(int argc, char** argp)
          // have nathan create an account
          std::string new_account_name = "client" + std::to_string(i);
          graphene::wallet::brain_key_info bki = con.wallet_api_ptr->suggest_brain_key();
-         signed_transaction create_acct_tx = con.wallet_api_ptr->create_account_with_brain_key(bki.brain_priv_key, new_account_name, "nathan", "nathan", true);
+         signed_transaction create_acct_tx = con.wallet_api_ptr->create_account_with_brain_key(bki.brain_priv_key, 
+               new_account_name, "nathan", "nathan", true);
          // transfer CORE from nathan to this new client
          con.wallet_api_ptr->transfer("nathan", new_account_name, "1000000", "BTS", "", true);
          unsigned int server_number = i % num_servers;
@@ -446,7 +496,7 @@ int main(int argc, char** argp)
          c->connect();
          c->setDirectory(clients);
          c->start_message_loop();
-         c->do_random_actions_every( std::chrono::milliseconds(1) );
+         c->do_random_actions_every( client_message_loop_speed );
       }
       // now wait until the user wants to stop
       fc::promise<int>::ptr exit_promise = new fc::promise<int>("UNIX Signal Handler");
@@ -467,11 +517,11 @@ int main(int argc, char** argp)
    } 
    catch (fc::exception& ex)
    {
-      std::cerr << "FC Exception thrown: " << ex.to_detail_string(fc::log_level(fc::log_level::all)) << "\n";
+      std::cerr << "Main Thread: FC Exception thrown: " << ex.to_detail_string(fc::log_level(fc::log_level::all)) << "\n";
    }
    catch (...)
    {
-      std::cerr << "Uncaught exception thrown.\n";
+      std::cerr << "Main Thread: Uncaught exception thrown.\n";
    }
    return 0;
 }
