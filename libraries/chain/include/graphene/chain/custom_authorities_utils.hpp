1d592d1f7b3906a3767a342f638950f20d3db92e
Roman Mokych <roma@apriorit.com>
2018-12-06 09:45:27 +0200

    Fixed spaces in the new files.

--- libraries/chain/include/graphene/chain/custom_authorities_utils.hpp
+++ libraries/chain/include/graphene/chain/custom_authorities_utils.hpp
@@ -26,88 +26,88 @@
 namespace graphene { namespace chain {
 
 typedef fc::static_variant<asset, account_id_type, extensions_type, future_extensions, public_key_type, time_point_sec, bool > generic_member;
-    
+	
 template <typename T, typename Action>
 struct member_visitor
 {
-    member_visitor(const std::string& member_name,  const Action& action, const T& object)
-    : m_member_name(member_name)
-    , m_action(action)
-    , m_object(object)
-    {}
-    
-    typedef void result_type;
-    
-    template<typename Member, class Class, Member (Class::*member)>
-    void operator () ( const char* name ) const
-    {
-        if (name == m_member_name)
-        {
-            m_action(m_object.*member);
-        }
-    }
-    
+	member_visitor(const std::string& member_name,  const Action& action, const T& object)
+	: m_member_name(member_name)
+	, m_action(action)
+	, m_object(object)
+	{}
+	
+	typedef void result_type;
+	
+	template<typename Member, class Class, Member (Class::*member)>
+	void operator () ( const char* name ) const
+	{
+		if (name == m_member_name)
+		{
+			m_action(m_object.*member);
+		}
+	}
+	
 private:
-    const std::string m_member_name;
-    Action m_action;
-    T m_object;
+	const std::string m_member_name;
+	Action m_action;
+	T m_object;
 };
 
 template <typename Action>
 class operation_member_visitor
 {
 public:
-    operation_member_visitor(const std::string& member_name,  const Action& action)
-    : m_member_name(member_name)
-    , m_action(action)
-    {}
-    
-    typedef void result_type;
-    
-    template <typename Operation>
-    void operator () (const Operation& op) const
-    {
-        member_visitor<Operation, Action> vistor(m_member_name, m_action, op);
-        fc::reflector<Operation>::visit(vistor);
-    }
-    
+	operation_member_visitor(const std::string& member_name,  const Action& action)
+	: m_member_name(member_name)
+	, m_action(action)
+	{}
+	
+	typedef void result_type;
+	
+	template <typename Operation>
+	void operator () (const Operation& op) const
+	{
+		member_visitor<Operation, Action> vistor(m_member_name, m_action, op);
+		fc::reflector<Operation>::visit(vistor);
+	}
+	
 private:
-    const std::string m_member_name;
-    Action m_action;
+	const std::string m_member_name;
+	Action m_action;
 };
 
 template <class T>
 bool is_equal(const T& left, const T& right)
 {
-    FC_ASSERT(false);
+	FC_ASSERT(false);
 }
-    
+	
 bool is_equal(const asset& left, const asset& right)
 {
-    return left == right;
+	return left == right;
 }
 
 bool is_equal(const account_id_type& left, const account_id_type& right)
 {
-    return left == right;
+	return left == right;
 }
-    
+	
 template <typename T>
 const T& get(const generic_member& variant)
 {
-    FC_ASSERT(false);
+	FC_ASSERT(false);
 }
 
 template <>
 const asset& get<asset>(const generic_member& variant)
 {
-    return variant.get<asset>();
+	return variant.get<asset>();
 }
 
 template <>
 const account_id_type& get<account_id_type>(const generic_member& variant)
 {
-    return variant.get<account_id_type>();
+	return variant.get<account_id_type>();
 }
-    
+	
 } } 
