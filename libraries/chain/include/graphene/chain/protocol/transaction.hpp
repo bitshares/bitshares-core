7a88dff0a6f25db0d850003101f6fbde4ad4b8d0
John Jones <jmjatlanta@gmail.com>
2019-01-08 09:27:53 -0500

    add virtual destructors to classes derived from transaction

--- libraries/chain/include/graphene/chain/protocol/transaction.hpp
+++ libraries/chain/include/graphene/chain/protocol/transaction.hpp
@@ -128,6 +128,7 @@ namespace graphene { namespace chain {
    public:
       signed_transaction( const transaction& trx = transaction() )
          : transaction(trx){}
+      virtual ~signed_transaction() = default;
 
       /** signs and appends to signatures */
       const signature_type& sign( const private_key_type& key, const chain_id_type& chain_id );
@@ -206,6 +207,7 @@ namespace graphene { namespace chain {
       precomputable_transaction() {}
       precomputable_transaction( const signed_transaction& tx ) : signed_transaction(tx) {};
       precomputable_transaction( signed_transaction&& tx ) : signed_transaction( std::move(tx) ) {};
+      virtual ~precomputable_transaction() = default;
 
       virtual const transaction_id_type&       id()const override;
       virtual void                             validate()const override;
@@ -239,6 +241,7 @@ namespace graphene { namespace chain {
    {
       processed_transaction( const signed_transaction& trx = signed_transaction() )
          : precomputable_transaction(trx){}
+      virtual ~processed_transaction() = default;
 
       vector<operation_result> operation_results;
 
