dc901237c53c7af6f66f95e8227c0ab17032b5fd
John Jones <jmjatlanta@gmail.com>
2019-02-11 06:19:26 -0500

    Added missing softfork code

--- libraries/chain/proposal_evaluator.cpp
+++ libraries/chain/proposal_evaluator.cpp
@@ -38,18 +38,27 @@ namespace detail {
 struct proposal_operation_hardfork_visitor
 {
    typedef void result_type;
+   const database& db;
    const fc::time_point_sec block_time;
    const fc::time_point_sec next_maintenance_time;
 
-   proposal_operation_hardfork_visitor( const fc::time_point_sec bt, const fc::time_point_sec nmt )
-   : block_time(bt), next_maintenance_time(nmt) {}
+   proposal_operation_hardfork_visitor( const database& _db, const fc::time_point_sec bt )
+   : db( _db ), block_time(bt), next_maintenance_time( db.get_dynamic_global_properties().next_maintenance_time ) {}
 
    template<typename T>
    void operator()(const T &v) const {}
 
-   // TODO review and cleanup code below after hard fork
-   // hf_834
    void operator()(const graphene::chain::call_order_update_operation &v) const {
+
+      // TODO If this never ASSERTs before HF 1465, it can be removed
+      FC_ASSERT( block_time > SOFTFORK_CORE_1465_TIME && block_time < HARDFORK_CORE_1465_TIME
+            && v.delta_debt.amount > 0 && v.delta_debt.asset_id != asset_id_type(113) // CNY
+            && v.delta_debt.asset_id( db ).bitasset_data_id
+            && !(*( v.delta_debt.asset_id( db ).bitasset_data_id))( db ).is_prediction_market
+            , "Soft fork - preventing proposal with call_order_update!" );
+   
+      // TODO review and cleanup code below after hard fork
+      // hf_834
       if (next_maintenance_time <= HARDFORK_CORE_834_TIME) {
          FC_ASSERT( !v.extensions.value.target_collateral_ratio.valid(),
                     "Can not set target_collateral_ratio in call_order_update_operation before hardfork 834." );
@@ -171,8 +180,7 @@ void_result proposal_create_evaluator::do_evaluate(const proposal_create_operati
 
    // Calling the proposal hardfork visitor
    const fc::time_point_sec block_time = d.head_block_time();
-   const fc::time_point_sec next_maint_time = d.get_dynamic_global_properties().next_maintenance_time;
-   proposal_operation_hardfork_visitor vtor( block_time, next_maint_time );
+   proposal_operation_hardfork_visitor vtor( d, block_time );
    vtor( o );
    if( block_time < HARDFORK_CORE_214_TIME )
    { // cannot be removed after hf, unfortunately
