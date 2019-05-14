1b53bffc514dd64aa8d9020c417d21a57c7f676c
John Jones <jmjatlanta@gmail.com>
2019-05-14 14:34:37 -0500

    2nd attempt to satisfy travis build/clang

--- tests/tests/p2p_node_tests.cpp
+++ tests/tests/p2p_node_tests.cpp
@@ -175,7 +175,7 @@ public:
    std::shared_ptr<graphene::net::message> message_received;
    void send_message(const graphene::net::message& message_to_send, size_t message_send_time_field_offset = (size_t)-1) override
    {
-      if ( graphene::net::core_message_type_enum(message_to_send.msg_type) == graphene::net::address_message::type)
+      if ( message_to_send.msg_type == static_cast<uint32_t>(graphene::net::address_message::type))
       {
          // make a copy
          graphene::net::address_message m = message_to_send.as<graphene::net::address_message>();
