00d383deb07524f12ba356e398f5c626b99082be
Reed Allred <reed.allred@mx.com>
2019-07-07 14:16:44 -0600

    Fixing assignment

--- libraries/chain/account_evaluator.cpp
+++ libraries/chain/account_evaluator.cpp
@@ -429,7 +429,7 @@ void_result account_upgrade_evaluator::do_apply(const account_upgrade_evaluator:
          a.referrer = a.registrar = a.lifetime_referrer = a.get_id();
          a.lifetime_referrer_fee_percentage = GRAPHENE_DEFAULT_LIFETIME_REFERRER_PERCENT_OF_FEE;
          a.marketing_partner_fee_percentage = GRAPHENE_DEFAULT_MARKETING_PARTNER_PERCENT_OF_FEE;
-         a.marketing_partner_fee_percentage = GRAPHENE_DEFAULT_CHARITY_PERCENT_OF_FEE;
+         a.charity_fee_percentage = GRAPHENE_DEFAULT_CHARITY_PERCENT_OF_FEE;
       } else if( a.is_annual_member(d.head_block_time()) ) {
          // Renew an annual subscription that's still in effect.
          FC_ASSERT( d.head_block_time() <= HARDFORK_613_TIME );
