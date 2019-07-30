f85ebb11cf13b730b52c45b2ca6a3f411e847a04
abitmore <abitmore@users.noreply.github.com>
2019-07-30 17:17:51 -0400

    Wrap long lines

--- libraries/chain/exceptions.cpp
+++ libraries/chain/exceptions.cpp
@@ -29,7 +29,7 @@ namespace graphene { namespace chain {
 
    // Internal exceptions
 
-   FC_IMPLEMENT_DERIVED_EXCEPTION( internal_exception, graphene::chain::chain_exception, 3990000, "internal exception" )
+   FC_IMPLEMENT_DERIVED_EXCEPTION( internal_exception, chain_exception, 3990000, "internal exception" )
 
    GRAPHENE_IMPLEMENT_INTERNAL_EXCEPTION( verify_auth_max_auth_exceeded, 1, "Exceeds max authority fan-out" )
    GRAPHENE_IMPLEMENT_INTERNAL_EXCEPTION( verify_auth_account_not_found, 2, "Auth account not found" )
@@ -39,22 +39,31 @@ namespace graphene { namespace chain {
 
    FC_IMPLEMENT_EXCEPTION( chain_exception, 3000000, "blockchain exception" )
 
-   FC_IMPLEMENT_DERIVED_EXCEPTION( database_query_exception,     chain_exception, 3010000, "database query exception" )
-   FC_IMPLEMENT_DERIVED_EXCEPTION( block_validate_exception,     chain_exception, 3020000, "block validation exception" )
-   FC_IMPLEMENT_DERIVED_EXCEPTION( transaction_process_exception,chain_exception, 3030000, "transaction processing exception" )
-   FC_IMPLEMENT_DERIVED_EXCEPTION( operation_validate_exception, chain_exception, 3040000, "operation validation exception" )
-   FC_IMPLEMENT_DERIVED_EXCEPTION( operation_evaluate_exception, chain_exception, 3050000, "operation evaluation exception" )
-   FC_IMPLEMENT_DERIVED_EXCEPTION( utility_exception,            chain_exception, 3060000, "utility method exception" )
-   FC_IMPLEMENT_DERIVED_EXCEPTION( undo_database_exception,      chain_exception, 3070000, "undo database exception" )
+   FC_IMPLEMENT_DERIVED_EXCEPTION( database_query_exception,     chain_exception, 3010000,
+                                   "database query exception" )
+   FC_IMPLEMENT_DERIVED_EXCEPTION( block_validate_exception,     chain_exception, 3020000,
+                                   "block validation exception" )
+   FC_IMPLEMENT_DERIVED_EXCEPTION( transaction_process_exception,chain_exception, 3030000,
+                                   "transaction processing exception" )
+   FC_IMPLEMENT_DERIVED_EXCEPTION( operation_validate_exception, chain_exception, 3040000,
+                                   "operation validation exception" )
+   FC_IMPLEMENT_DERIVED_EXCEPTION( operation_evaluate_exception, chain_exception, 3050000,
+                                   "operation evaluation exception" )
+   FC_IMPLEMENT_DERIVED_EXCEPTION( utility_exception,            chain_exception, 3060000,
+                                   "utility method exception" )
+   FC_IMPLEMENT_DERIVED_EXCEPTION( undo_database_exception,      chain_exception, 3070000,
+                                   "undo database exception" )
    FC_IMPLEMENT_DERIVED_EXCEPTION( unlinkable_block_exception,   chain_exception, 3080000, "unlinkable block" )
    FC_IMPLEMENT_DERIVED_EXCEPTION( black_swan_exception,         chain_exception, 3090000, "black swan" )
    FC_IMPLEMENT_DERIVED_EXCEPTION( plugin_exception,             chain_exception, 3100000, "plugin exception" )
 
    FC_IMPLEMENT_DERIVED_EXCEPTION( insufficient_feeds,           chain_exception, 37006, "insufficient feeds" )
 
-   FC_IMPLEMENT_DERIVED_EXCEPTION( duplicate_transaction,        transaction_process_exception, 3030001, "duplicate transaction" )
+   FC_IMPLEMENT_DERIVED_EXCEPTION( duplicate_transaction,        transaction_process_exception, 3030001,
+                                   "duplicate transaction" )
 
-   FC_IMPLEMENT_DERIVED_EXCEPTION( pop_empty_chain,              undo_database_exception, 3070001, "there are no blocks to pop" )
+   FC_IMPLEMENT_DERIVED_EXCEPTION( pop_empty_chain,              undo_database_exception, 3070001,
+                                   "there are no blocks to pop" )
 
    GRAPHENE_IMPLEMENT_OP_BASE_EXCEPTIONS( transfer );
    GRAPHENE_IMPLEMENT_OP_EVALUATE_EXCEPTION( from_account_not_whitelisted, transfer, 1, "owner mismatch" )
@@ -80,13 +89,16 @@ namespace graphene { namespace chain {
    GRAPHENE_IMPLEMENT_OP_EVALUATE_EXCEPTION( owner_mismatch, limit_order_cancel, 2, "Order owned by someone else" )
 
    GRAPHENE_IMPLEMENT_OP_BASE_EXCEPTIONS( call_order_update );
-   GRAPHENE_IMPLEMENT_OP_EVALUATE_EXCEPTION( unfilled_margin_call, call_order_update, 1, "Updating call order would trigger a margin call that cannot be fully filled" )
+   GRAPHENE_IMPLEMENT_OP_EVALUATE_EXCEPTION( unfilled_margin_call, call_order_update, 1,
+         "Updating call order would trigger a margin call that cannot be fully filled" )
 
    GRAPHENE_IMPLEMENT_OP_BASE_EXCEPTIONS( account_create );
    GRAPHENE_IMPLEMENT_OP_EVALUATE_EXCEPTION( max_auth_exceeded, account_create, 1, "Exceeds max authority fan-out" )
    GRAPHENE_IMPLEMENT_OP_EVALUATE_EXCEPTION( auth_account_not_found, account_create, 2, "Auth account not found" )
-   GRAPHENE_IMPLEMENT_OP_EVALUATE_EXCEPTION( buyback_incorrect_issuer, account_create, 3, "Incorrect issuer specified for account" )
-   GRAPHENE_IMPLEMENT_OP_EVALUATE_EXCEPTION( buyback_already_exists, account_create, 4, "Cannot create buyback for asset which already has buyback" )
+   GRAPHENE_IMPLEMENT_OP_EVALUATE_EXCEPTION( buyback_incorrect_issuer, account_create, 3,
+         "Incorrect issuer specified for account" )
+   GRAPHENE_IMPLEMENT_OP_EVALUATE_EXCEPTION( buyback_already_exists, account_create, 4,
+         "Cannot create buyback for asset which already has buyback" )
    GRAPHENE_IMPLEMENT_OP_EVALUATE_EXCEPTION( buyback_too_many_markets, account_create, 5, "Too many buyback markets" )
 
    GRAPHENE_IMPLEMENT_OP_BASE_EXCEPTIONS( account_update );
@@ -114,7 +126,8 @@ namespace graphene { namespace chain {
 
    GRAPHENE_IMPLEMENT_OP_BASE_EXCEPTIONS( proposal_create );
    GRAPHENE_IMPLEMENT_OP_EVALUATE_EXCEPTION( review_period_required, proposal_create, 1, "review_period required" )
-   GRAPHENE_IMPLEMENT_OP_EVALUATE_EXCEPTION( review_period_insufficient, proposal_create, 2, "review_period insufficient" )
+   GRAPHENE_IMPLEMENT_OP_EVALUATE_EXCEPTION( review_period_insufficient, proposal_create, 2,
+         "review_period insufficient" )
 
    //GRAPHENE_IMPLEMENT_OP_BASE_EXCEPTIONS( proposal_update );
    //GRAPHENE_IMPLEMENT_OP_BASE_EXCEPTIONS( proposal_delete );
@@ -139,7 +152,8 @@ namespace graphene { namespace chain {
    GRAPHENE_IMPLEMENT_OP_EVALUATE_EXCEPTION( not_permitted, override_transfer, 1, "not permitted" )
 
    GRAPHENE_IMPLEMENT_OP_BASE_EXCEPTIONS( blind_transfer );
-   GRAPHENE_IMPLEMENT_OP_EVALUATE_EXCEPTION( unknown_commitment, blind_transfer, 1, "Attempting to claim an unknown prior commitment" );
+   GRAPHENE_IMPLEMENT_OP_EVALUATE_EXCEPTION( unknown_commitment, blind_transfer, 1,
+         "Attempting to claim an unknown prior commitment" );
 
    //GRAPHENE_IMPLEMENT_OP_BASE_EXCEPTIONS( transfer_from_blind_operation )
    //GRAPHENE_IMPLEMENT_OP_BASE_EXCEPTIONS( asset_claim_fees_operation )
