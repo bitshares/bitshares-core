nathanhourt/bitshares-2

Ref #210: Add hardfork guards
Hardfork guards are complex for this issue, because we can't access
chain time in the operation's get_required_active_authorities() method.

There was no apparent 'good' way to solve this, so I settled for a
fairly nasty hack which seemed the least bad of bad options. Add a
boolean parameter to the verify_authority() functions determining
whether the custom_operation::required_auths field should be ignored or
not, and call these functions setting the boolean appropriately for
whether we have passed the hardfork or not. This works because chain
time is available at the verify_authority() call sites.

libraries/chain/db_block.cpp
@@ -631,7 +631,18 @@ processed_transaction database::_apply_transaction(const signed_transaction& trx
    {
       auto get_active = [&]( account_id_type id ) { return &id(*this).active; };
       auto get_owner  = [&]( account_id_type id ) { return &id(*this).owner;  };
-      trx.verify_authority( chain_id, get_active, get_owner, get_global_properties().parameters.max_authority_depth );
+
+      // See bitshares-core issue #210 for discussion
+      // The custom_operation::get_required_active_authorities() method initially failed to report the authorities
+      // from the custom_operaton::required_auths field. This was a bug. It's a simple fix in that method, but the
+      // fix is a hardfork, and thus we need a hardfork guard. Since that method cannot access chain time, we must
+      // implement the guard here, and skip the call to get_required_active_authorities() prior to the hardfork.
+      // Therefore, if the head_block_time() is prior to the 210 hardfork, we ignore the required auths specified
+      // by the custom_operation.
+      bool ignore_custom_operation_required_auths = (head_block_time() <= HARDFORK_CORE_210_TIME);
+
+      trx.verify_authority(chain_id, get_active, get_owner, ignore_custom_operation_required_auths,
+                           get_global_properties().parameters.max_authority_depth);
    }
 
    //Skip all manner of expiration and TaPoS checking if we're on block 1; It's impossible that the transaction is

bitshares/bitshares-core

libraries/chain/db_block.cpp
@@ -646,11 +646,19 @@ processed_transaction database::_apply_transaction(const signed_transaction& trx
       bool allow_non_immediate_owner = ( head_block_time() >= HARDFORK_CORE_584_TIME );
       auto get_active = [&]( account_id_type id ) { return &id(*this).active; };
       auto get_owner  = [&]( account_id_type id ) { return &id(*this).owner;  };
-      trx.verify_authority( chain_id,
-                            get_active,
-                            get_owner,
-                            allow_non_immediate_owner,
-                            get_global_properties().parameters.max_authority_depth );
+
+      // See bitshares-core issue #210 for discussion
+      // The custom_operation::get_required_active_authorities() method initially failed to report the authorities
+      // from the custom_operaton::required_auths field. This was a bug. It's a simple fix in that method, but the
+      // fix is a hardfork, and thus we need a hardfork guard. Since that method cannot access chain time, we must
+      // implement the guard here, and skip the call to get_required_active_authorities() prior to the hardfork.
+      // Therefore, if the head_block_time() is prior to the 210 hardfork, we ignore the required auths specified
+      // by the custom_operation.
+      bool ignore_custom_operation_required_auths = (head_block_time() <= HARDFORK_CORE_210_TIME);
+
+      trx.verify_authority(chain_id, get_active, get_owner, allow_non_immediate_owner,
+                           ignore_custom_operation_required_auths,
+                           get_global_properties().parameters.max_authority_depth);
    }
 
    //Skip all manner of expiration and TaPoS checking if we're on block 1; It's impossible that the transaction is