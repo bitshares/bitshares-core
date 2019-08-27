975a7becde0d8cffc65a7844e428c76c84578fa3
beejianing <ddpfvt@163.com>
2019-08-27 10:34:49 +0800

    modify the indentation size and delete the error file

--- libraries/plugins/query_txid/include/graphene/query_txid/query_txid_plugin.hpp
+++ libraries/plugins/query_txid/include/graphene/query_txid/query_txid_plugin.hpp
@@ -36,23 +36,23 @@ class query_txid_plugin_impl;
 }
 class query_txid_plugin : public graphene::app::plugin
 {
-  public:
-    query_txid_plugin();
-    virtual ~query_txid_plugin();
+   public:
+      query_txid_plugin();
+      virtual ~query_txid_plugin();
 
-    std::string plugin_name() const override;
+      std::string plugin_name() const override;
 
-    virtual void plugin_set_program_options(
-        boost::program_options::options_description &cli,
-        boost::program_options::options_description &cfg) override;
+      virtual void plugin_set_program_options(
+      boost::program_options::options_description &cli,
+      boost::program_options::options_description &cfg) override;
 
-    virtual void plugin_initialize(const boost::program_options::variables_map &options) override;
-    virtual void plugin_startup() override;
+      virtual void plugin_initialize(const boost::program_options::variables_map &options) override;
+      virtual void plugin_startup() override;
 
-    static optional<trx_entry_object> query_trx_by_id(std::string txid);
+      static optional<trx_entry_object> query_trx_by_id(std::string txid);
 
-    friend class detail::query_txid_plugin_impl;
+      friend class detail::query_txid_plugin_impl;
 
-    std::unique_ptr<detail::query_txid_plugin_impl> my;
+      std::unique_ptr<detail::query_txid_plugin_impl> my;
 };
 } }// graphene::query_txid 
