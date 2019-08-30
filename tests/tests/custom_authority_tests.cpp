dd75b136cc9bbbf6b42417f2b9981a5e02d02ee4
Michel Santos <MichelSantos@users.noreply.github.com>
2019-08-30 16:42:26 -0400

    BSIP 40: Unit test comments

--- tests/tests/custom_authority_tests.cpp
+++ tests/tests/custom_authority_tests.cpp
@@ -77,86 +77,323 @@ void expect_exception_string(const string& s, Expression e) {
 
 BOOST_AUTO_TEST_CASE(restriction_predicate_tests) { try {
    using namespace graphene::protocol;
+   //////
+   // Create a restriction that authorizes transfers only made to Account ID 12
+   //////
    vector<restriction> restrictions;
-   transfer_operation transfer;
-
    auto to_index = member_index<transfer_operation>("to");
    restrictions.emplace_back(to_index, FUNC(eq), account_id_type(12));
+
+   //////
+   // Create an operation that transfers to Account ID 0
+   // This should violate the restriction
+   //////
+   transfer_operation transfer;
+   // Check that the proposed operation to account ID 0 is not compliant with the restriction to account ID 12
    BOOST_CHECK(get_restriction_predicate(restrictions, operation::tag<transfer_operation>::value)(transfer) == false);
+   // Inspect the reasons why the proposed operation was rejected
+   // The rejection path will reference portions of the restrictions
+   //[
+   //  {
+   //    "member_index": 2,
+   //    "restriction_type": 0,
+   //    "argument": [
+   //      7,
+   //      "1.2.12"
+   //    ],
+   //    "extensions": []
+   //  }
+   //]
    BOOST_CHECK(get_restriction_predicate(restrictions, operation::tag<transfer_operation>::value)(transfer)
                .rejection_path.size() == 2);
+   // Index 0 (the outer-most) rejection path refers to the first and only restriction
    BOOST_CHECK(get_restriction_predicate(restrictions, operation::tag<transfer_operation>::value)(transfer)
                .rejection_path[0].get<size_t>() == 0);
+   // Index 1 (the inner-most) rejection path refers to the first and only argument for an account ID of 1.2.12
    BOOST_CHECK(get_restriction_predicate(restrictions, operation::tag<transfer_operation>::value)(transfer)
                .rejection_path[1].get<predicate_result::rejection_reason>() == predicate_result::predicate_was_false);
+
+   //////
+   // Create an operation that transfer to Account ID 12
+   // This should satisfy the restriction
+   //////
    transfer.to = account_id_type(12);
    BOOST_CHECK(get_restriction_predicate(restrictions, operation::tag<transfer_operation>::value)(transfer) == true);
    BOOST_CHECK(get_restriction_predicate(restrictions, operation::tag<transfer_operation>::value)(transfer)
                .rejection_path.size() == 0);
 
+
+   //////
+   // Create an INVALID restriction that references an invalid member index
+   // (Index 6 is greater than the highest 0-based index of 5)
+   // of the transfer operation
+   //////
    restrictions.front() = restriction(fc::typelist::length<fc::reflector<transfer_operation>::native_members>(),
                                       FUNC(eq), account_id_type(12));
-   BOOST_CHECK_THROW(get_restriction_predicate(restrictions, operation::tag<transfer_operation>::value),
-                     fc::assert_exception);
-   restrictions.front() = restriction(to_index, FUNC(eq), asset_id_type(12));
+   //[
+   //  {
+   //    "member_index": 6,
+   //    "restriction_type": 0,
+   //    "argument": [
+   //      7,
+   //      "1.2.12"
+   //    ],
+   //    "extensions": []
+   //  }
+   //]
+   //
+   // This restriction should throw an exception related to an invalid member index
+   //   10 assert_exception: Assert Exception
+   //   r.member_index < typelist::length<member_list>(): Invalid member index 6 for object graphene::protocol::transfer_operation
+   //           {"I":6,"O":"graphene::protocol::transfer_operation"}
+   //   th_a  restriction_predicate.hxx:493 create_field_predicate
    BOOST_CHECK_THROW(get_restriction_predicate(restrictions, operation::tag<transfer_operation>::value),
                      fc::assert_exception);
 
+
+   //////
+   // Create an INVALID restriction that compares a transfer operation's account ID type to an asset ID type
+   //////
+   restrictions.front() = restriction(to_index, FUNC(eq), asset_id_type(12));
+   //[
+   //  {
+   //    "member_index": 2,
+   //    "restriction_type": 0,
+   //    "argument": [
+   //      8,
+   //      "1.3.12"
+   //    ],
+   //    "extensions": []
+   //  }
+   //]
+   //
+   // This restriction should throw an exception related to invalid type
+   //   10 assert_exception: Assert Exception
+   //   Invalid types for predicate
+   //   {}
+   //   th_a  restriction_predicate.hxx:147 predicate_invalid
+   //
+   //   {"fc::get_typename<Field>::name()":"graphene::protocol::account_id_type","func":"func_eq","arg":[8,"1.3.12"]}
+   //   th_a  restriction_predicate.hxx:476 create_predicate_function
+   BOOST_CHECK_THROW(get_restriction_predicate(restrictions, operation::tag<transfer_operation>::value),
+                     fc::assert_exception);
+
+   //////
+   // Create a restriction such that the operation fee must be paid with Asset ID 0
+   //////
    auto fee_index = member_index<transfer_operation>("fee");
    auto asset_id_index = member_index<asset>("asset_id");
    restrictions.front() = restriction(fee_index, FUNC(attr),
                                       vector<restriction>{restriction(asset_id_index, FUNC(eq), asset_id_type(0))});
+
+   //////
+   // Check the transfer operation that pays the fee with Asset ID 0
+   // This should satisfy the restriction.
+   //////
    BOOST_CHECK(get_restriction_predicate(restrictions, operation::tag<transfer_operation>::value)(transfer) == true);
    BOOST_CHECK(get_restriction_predicate(restrictions, operation::tag<transfer_operation>::value)(transfer)
                .rejection_path.size() == 0);
+
+   //////
+   // Change the restriction such that the operation fee must be paid with Asset ID 1
+   //////
    restrictions.front().argument.get<vector<restriction>>().front().argument = asset_id_type(1);
+   //[
+   //  {
+   //    "member_index": 0,
+   //    "restriction_type": 10,
+   //    "argument": [
+   //      39,
+   //      [
+   //        {
+   //          "member_index": 1,
+   //          "restriction_type": 0,
+   //          "argument": [
+   //            8,
+   //            "1.3.1"
+   //          ],
+   //          "extensions": []
+   //        }
+   //      ]
+   //    ],
+   //    "extensions": []
+   //  }
+   //]
+
+   //////
+   // Check the transfer operation that pays the fee with Asset ID 0 against the restriction.
+   // This should violate the restriction.
+   //////
    BOOST_CHECK(get_restriction_predicate(restrictions, operation::tag<transfer_operation>::value)(transfer) == false);
+   // Inspect the reasons why the proposed operation was rejected
+   // The rejection path will reference portions of the restrictions
    BOOST_CHECK(get_restriction_predicate(restrictions, operation::tag<transfer_operation>::value)(transfer)
                .rejection_path.size() == 3);
+   // Index 0 (the outer-most) rejection path refers to the first and only restriction
    BOOST_CHECK(get_restriction_predicate(restrictions, operation::tag<transfer_operation>::value)(transfer)
                .rejection_path[0].get<size_t>() == 0);
+   // Index 1 rejection path refers to the first and only argument of the restriction
    BOOST_CHECK(get_restriction_predicate(restrictions, operation::tag<transfer_operation>::value)(transfer)
                .rejection_path[1].get<size_t>() == 0);
+   // Index 2 (the inner-most) rejection path refers to the first and only argument
    BOOST_CHECK(get_restriction_predicate(restrictions, operation::tag<transfer_operation>::value)(transfer)
                .rejection_path[2].get<predicate_result::rejection_reason>() == predicate_result::predicate_was_false);
+
+   //////
+   // Create a restriction that authorizes transfers only to Account ID 12
+   //////
    restrictions.emplace_back(to_index, FUNC(eq), account_id_type(12));
+   //[
+   //  {
+   //    "member_index": 0,
+   //    "restriction_type": 10,
+   //    "argument": [
+   //      39,
+   //      [
+   //        {
+   //          "member_index": 1,
+   //          "restriction_type": 0,
+   //          "argument": [
+   //            8,
+   //            "1.3.1"
+   //          ],
+   //          "extensions": []
+   //        }
+   //      ]
+   //    ],
+   //    "extensions": []
+   //  },
+   //  {
+   //    "member_index": 2,
+   //    "restriction_type": 0,
+   //    "argument": [
+   //      7,
+   //      "1.2.12"
+   //    ],
+   //    "extensions": []
+   //  }
+   //]
+
+   //////
+   // Create a transfer operation that authorizes transfer to Account ID 12
+   // This operation should satisfy the restriction
+   //////
    transfer.to = account_id_type(12);
    transfer.fee.asset_id = asset_id_type(1);
    BOOST_CHECK(get_restriction_predicate(restrictions, operation::tag<transfer_operation>::value)(transfer) == true);
    BOOST_CHECK(get_restriction_predicate(restrictions, operation::tag<transfer_operation>::value)(transfer)
                .rejection_path.size() == 0);
+
+   //////
+   // Create a transfer operation that transfers to Account ID 10
+   // This operation should violate the restriction
+   //////
    transfer.to = account_id_type(10);
    BOOST_CHECK(get_restriction_predicate(restrictions, operation::tag<transfer_operation>::value)(transfer) == false);
+   // Inspect the reasons why the proposed operation was rejected
+   // The rejection path will reference portions of the restrictions
    BOOST_CHECK(get_restriction_predicate(restrictions, operation::tag<transfer_operation>::value)(transfer)
                .rejection_path.size() == 2);
+   // Index 0 (the outer-most) rejection path refers to the first and only restriction
    BOOST_CHECK(get_restriction_predicate(restrictions, operation::tag<transfer_operation>::value)(transfer)
                .rejection_path[0].get<size_t>() == 1);
+   // Index 1 (the inner-most) rejection path refers to the first and only argument
    BOOST_CHECK(get_restriction_predicate(restrictions, operation::tag<transfer_operation>::value)(transfer)
                .rejection_path[1].get<predicate_result::rejection_reason>() == predicate_result::predicate_was_false);
 
-   account_update_operation update;
+   //////
+   // Create a restriction where the ext.owner_special_authority field is unspecified
+   //////
    restrictions.clear();
    auto extensions_index = member_index<account_update_operation>("extensions");
    auto authority_index = member_index<account_update_operation::ext>("owner_special_authority");
    restrictions.emplace_back(extensions_index, FUNC(attr),
                              vector<restriction>{restriction(authority_index, FUNC(eq), void_t())});
+   //[
+   //  {
+   //    "member_index": 5,
+   //    "restriction_type": 10,
+   //    "argument": [
+   //      39,
+   //      [
+   //        {
+   //          "member_index": 1,
+   //          "restriction_type": 0,
+   //          "argument": [
+   //            0,
+   //            {}
+   //          ],
+   //          "extensions": []
+   //        }
+   //      ]
+   //    ],
+   //    "extensions": []
+   //  }
+   //]
    auto predicate = get_restriction_predicate(restrictions, operation::tag<account_update_operation>::value);
+
+   //////
+   // Create an account update operation without any owner_special_authority extension
+   //////
+   account_update_operation update;
+   // The transfer operation should violate the restriction because it does not have an ext field
    BOOST_CHECK_THROW(predicate(transfer), fc::assert_exception);
+   // The update operation should satisfy the restriction
    BOOST_CHECK(predicate(update) == true);
    BOOST_CHECK(predicate(update).rejection_path.size() == 0);
+
+   //////
+   // Change the update operation to include an owner_special_authority
+   // This should violate the restriction
+   //////
    update.extensions.value.owner_special_authority = special_authority();
    BOOST_CHECK(predicate(update) == false);
    BOOST_CHECK_EQUAL(predicate(update).rejection_path.size(), 3);
+   // Index 0 (the outer-most) rejection path refers to the first and only restriction
    BOOST_CHECK(predicate(update).rejection_path[0].get<size_t>() == 0);
+   // Index 1 rejection path refers to the first and only argument of the restriction
    BOOST_CHECK(predicate(update).rejection_path[1].get<size_t>() == 0);
+   // Index 2 (the inner-most) rejection path refers to the first and only argument
    BOOST_CHECK(predicate(update).rejection_path[2].get<predicate_result::rejection_reason>() ==
                predicate_result::predicate_was_false);
+
+   //////
+   // Change the restriction where the ext.owner_special_authority field must be specified
+   //////
    restrictions.front().argument.get<vector<restriction>>().front().restriction_type = FUNC(ne);
+   //[
+   //  {
+   //    "member_index": 5,
+   //    "restriction_type": 10,
+   //    "argument": [
+   //      39,
+   //      [
+   //        {
+   //          "member_index": 1,
+   //          "restriction_type": 1,
+   //          "argument": [
+   //            0,
+   //            {}
+   //          ],
+   //          "extensions": []
+   //        }
+   //      ]
+   //    ],
+   //    "extensions": []
+   //  }
+   //]
+
+   //////
+   // The update operation should satisfy the new restriction because the ext.owner_special_authority is specified
+   //////
    predicate = get_restriction_predicate(restrictions, operation::tag<account_update_operation>::value);
    BOOST_CHECK(predicate(update) == true);
 } FC_LOG_AND_RETHROW() }
 
 BOOST_AUTO_TEST_CASE(custom_auths) { try {
+   //////
+   // Initialize the test
+   //////
    generate_blocks(HARDFORK_BSIP_40_TIME);
    generate_blocks(5);
    db.modify(global_property_id_type()(db), [](global_property_object& gpo) {
@@ -167,6 +404,11 @@ BOOST_AUTO_TEST_CASE(custom_auths) { try {
    fund(alice, asset(1000*GRAPHENE_BLOCKCHAIN_PRECISION));
    fund(bob, asset(1000*GRAPHENE_BLOCKCHAIN_PRECISION));
 
+   //////
+   // Create a custom authority where Bob is authorized to transfer from Alice's account
+   // if and only if the transfer amount is less than 100 of Asset ID 0.
+   // This custom authority is NOT YET published.
+   //////
    custom_authority_create_operation op;
    op.account = alice.get_id();
    op.auth.add_authority(bob.get_id(), 1);
@@ -181,7 +423,42 @@ BOOST_AUTO_TEST_CASE(custom_auths) { try {
                           restriction(asset_amount_index, restriction::func_lt,
                                       int64_t(100*GRAPHENE_BLOCKCHAIN_PRECISION)),
                           restriction(assed_id_index, restriction::func_eq, asset_id_type(0))})};
+   //[
+   //  {
+   //    "member_index": 3,
+   //    "restriction_type": 10,
+   //    "argument": [
+   //      39,
+   //      [
+   //        {
+   //          "member_index": 0,
+   //          "restriction_type": 2,
+   //          "argument": [
+   //            2,
+   //            10000000
+   //          ],
+   //          "extensions": []
+   //        },
+   //        {
+   //          "member_index": 1,
+   //          "restriction_type": 0,
+   //          "argument": [
+   //            8,
+   //            "1.3.0"
+   //          ],
+   //          "extensions": []
+   //        }
+   //      ]
+   //    ],
+   //    "extensions": []
+   //  }
+   //]
 
+
+   //////
+   // Bob attempts to transfer 99 CORE from Alice's account
+   // This attempt should fail because it is attempted before the custom authority is published
+   //////
    transfer_operation top;
    top.to = bob.get_id();
    top.from = alice.get_id();
@@ -191,27 +468,41 @@ BOOST_AUTO_TEST_CASE(custom_auths) { try {
    // No custom auth yet; bob's transfer should reject
    BOOST_CHECK_THROW(PUSH_TX(db, trx), tx_missing_active_auth);
 
+   //////
+   // Alice publishes the custom authority
+   //////
    trx.clear();
    trx.operations = {op};
    sign(trx, alice_private_key);
-   // Alice publishes the custom authority
    PUSH_TX(db, trx);
 
    custom_authority_id_type auth_id =
            db.get_index_type<custom_authority_index>().indices().get<by_account_custom>().find(alice_id)->id;
 
+   //////
+   // Bob attempts to transfer 99 CORE from Alice's account
+   // This attempt should succeed because it is attempted after the custom authority is published
+   //////
    trx.clear();
    trx.operations = {top};
    sign(trx, bob_private_key);
-   // Now bob's transfer should succeed due to the custom authority
    PUSH_TX(db, trx);
 
+   //////
+   // Bob attempts to transfer 100 CORE from Alice's account
+   // This attempt should fail because it exceeds the authorized amount
+   //////
    trx.operations.front().get<transfer_operation>().amount.amount = 100*GRAPHENE_BLOCKCHAIN_PRECISION;
    trx.clear_signatures();
    sign(trx, bob_private_key);
    // If bob tries to transfer 100, it rejects because the restriction is strictly less than 100
    EXPECT_EXCEPTION_STRING("\"rejection_path\":[[0,0],[0,0],[2,\"predicate_was_false\"]]", [&] {PUSH_TX(db, trx);});
 
+   //////
+   // Update the custom authority so that Bob is authorized to transfer from Alice's account
+   // if and only if the transfer amount EXACTLY EQUALS 100 of Asset ID 0.
+   // This custom authority is NOT YET published.
+   //////
    op.restrictions.front().argument.get<vector<restriction>>().front().restriction_type = restriction::func_eq;
    custom_authority_update_operation uop;
    uop.account = alice.get_id();
@@ -221,11 +512,14 @@ BOOST_AUTO_TEST_CASE(custom_auths) { try {
    trx.clear();
    trx.operations = {uop};
    sign(trx, alice_private_key);
-   // Alice publishes an update to the custom authority, making the restriction require exactly 100
    PUSH_TX(db, trx);
 
    BOOST_CHECK(auth_id(db).get_restrictions() == uop.restrictions_to_add);
 
+   //////
+   // Bob attempts to transfer 99 CORE from Alice's account
+   // This attempt should fail because only transfers of 100 CORE are authorized
+   //////
    trx.clear();
    trx.operations = {top};
    trx.expiration += 5;
@@ -233,34 +527,46 @@ BOOST_AUTO_TEST_CASE(custom_auths) { try {
    // The transfer of 99 should reject because the requirement is for exactly 100
    EXPECT_EXCEPTION_STRING("\"rejection_path\":[[0,0],[0,0],[2,\"predicate_was_false\"]]", [&] {PUSH_TX(db, trx);});
 
+   //////
+   // Bob attempts to transfer 100 CORE from Alice's account
+   // This attempt should succeed because transfers of exactly 100 CORE are authorized by Alice
+   //////
    trx.operations.front().get<transfer_operation>().amount.amount = 100*GRAPHENE_BLOCKCHAIN_PRECISION;
    trx.clear_signatures();
    sign(trx, bob_private_key);
-   // A transfer of 100 should succeed
    PUSH_TX(db, trx);
    auto transfer = trx;
 
    generate_block();
 
+   //////
+   // Bob attempts to transfer 100 CORE from Alice's account AGAIN
+   // This attempt should succeed because there are no limits to the transfer size nor quantity
+   // besides the available CORE in Alice's account
+   //////
    trx.expiration += 5;
    trx.clear_signatures();
    sign(trx, bob_private_key);
-   // Another one should succeed
    PUSH_TX(db, trx);
 
+   //////
+   // Alice revokes the custom authority for Bob
+   //////
    custom_authority_delete_operation dop;
    dop.account = alice.get_id();
    dop.authority_to_delete = auth_id;
    trx.clear();
    trx.operations = {dop};
    sign(trx, alice_private_key);
-   // Alice deletes the custom authority
    PUSH_TX(db, trx);
 
+   //////
+   // Bob attempts to transfer 100 CORE from Alice's account
+   // This attempt should fail because it is attempted after the custom authority has been revoked
+   //////
    transfer.expiration += 10;
    transfer.clear_signatures();
    sign(transfer, bob_private_key);
-   // The transfer should no longer work
    BOOST_CHECK_THROW(PUSH_TX(db, transfer), tx_missing_active_auth);
 } FC_LOG_AND_RETHROW() }
 
