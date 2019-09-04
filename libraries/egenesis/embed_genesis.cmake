68f40d5733c3535bb355a04d5de1bd96cfd6f50e
Peter Conrad <conrad@quisquis.de>
2019-09-04 15:21:04 +0200

    Split genesis into 10k chunks

--- libraries/egenesis/embed_genesis.cmake
+++ libraries/egenesis/embed_genesis.cmake
@@ -16,9 +16,19 @@ string( LENGTH "${genesis_json}" genesis_json_length )
 string( REGEX REPLACE "(\"|\\\\)" "\\\\\\1" genesis_json_escaped "${genesis_json}" )
 string( REPLACE "\n" "\\n" genesis_json_escaped "${genesis_json_escaped}" )
 string( REPLACE "\t" "\\t" genesis_json_escaped "${genesis_json_escaped}" )
-set( genesis_json_array "\"${genesis_json_escaped}\"" )
-set( genesis_json_array_height 1 )
-set( genesis_json_array_width ${genesis_json_length} )
+
+# Split the escaped JSON string into chunks (each less than max string literal length supported by MSVC).
+# Chunks must not end with a backslash to avoid splitting escapes.
+string( RANDOM LENGTH 10000 _dots )
+string( REGEX REPLACE "." "." _dots "${_dots}" )
+string( REGEX MATCHALL "(${_dots}(.|..|...)?[^\\\\])" _chunks "${genesis_json_escaped}" )
+string( LENGTH "${_chunks}" _chunks_len )
+string( REGEX REPLACE ";" "" _seen "${_chunks}" )
+string( LENGTH "${_seen}" _seen )
+string( SUBSTRING "${genesis_json_escaped}" ${_seen} -1 _rest )
+string( REGEX REPLACE ";" "\",\n\"" genesis_json_array "${_chunks}" )
+set( genesis_json_array "\"${genesis_json_array}\",\n\"${_rest}\"" )
+set( genesis_json_array_height "${_chunks_len} - ${_seen} + 2" )
 
 configure_file( "${CMAKE_CURRENT_SOURCE_DIR}/egenesis_full.cpp.tmpl"
                 "${CMAKE_CURRENT_BINARY_DIR}/egenesis_full.cpp" )
