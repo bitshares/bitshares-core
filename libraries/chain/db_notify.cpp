43ef52176168d350bf2f3c31254fdfb1918a104c
dimfred <dimfred.1337@web.de>
2019-09-07 11:47:27 +0200

    add here?

--- libraries/chain/db_notify.cpp
+++ libraries/chain/db_notify.cpp
@@ -275,10 +275,10 @@ struct get_impacted_account_visitor
    }
    void operator()( const htlc_extend_operation& op )
    {
-      _impacted.insert( op.fee_payer() ); 
+      _impacted.insert( op.fee_payer() );
    }
-   void operator()( const htlc_refund_operation& op ) 
-   { 
+   void operator()( const htlc_refund_operation& op )
+   {
       _impacted.insert( op.fee_payer() );
    }
 };
@@ -375,6 +375,9 @@ void get_relevant_accounts( const object* obj, flat_set<account_id_type>& accoun
               accounts.insert( htlc_obj->transfer.to );
               break;
         }
+        /*
+            add voting and voteable here?
+        */
       }
    }
    else if( obj->id.space() == implementation_ids )
@@ -455,7 +458,7 @@ void database::notify_on_pending_transaction( const signed_transaction& tx )
 
 void database::notify_changed_objects()
 { try {
-   if( _undo_db.enabled() ) 
+   if( _undo_db.enabled() )
    {
       const auto& head_undo = _undo_db.head();
 
