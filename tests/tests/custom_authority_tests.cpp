63bfd3651302cc9d8a3f7fcd6b215286caac56a5
Michel Santos <MichelSantos@users.noreply.github.com>
2019-10-30 17:13:59 -0400

    BSIP 40: Unit test of less than or equal to (le) and greater than (gt) restriction

--- tests/tests/custom_authority_tests.cpp
+++ tests/tests/custom_authority_tests.cpp
@@ -3715,4 +3715,240 @@ BOOST_AUTO_TEST_CASE(custom_auths) { try {
    }
 
 
+   /**
+    * Test of greater than (gt) and less than or equal to (le) restriction on a field
+    * Test of CAA for htlc_extend_operation
+    *
+    * Scenario: Test of authorization of one account (alice) authorizing another account (bob)
+    * to extend an HTLC operation as long as the extension is within a specified duration
+    */
+   BOOST_AUTO_TEST_CASE(authorized_htlc_extension) {
+      try {
+         //////
+         // Initialize the blockchain
+         //////
+         time_point_sec LATER_HF_TIME
+                 = (HARDFORK_BSIP_40_TIME > HARDFORK_CORE_1468_TIME) ? HARDFORK_BSIP_40_TIME : HARDFORK_CORE_1468_TIME;
+         generate_blocks(LATER_HF_TIME);
+         generate_blocks(5);
+         set_expiration(db, trx);
+
+         // Initialize HTLC blockchain parameters
+         trx.clear();
+         set_htlc_committee_parameters();
+         generate_blocks(5);
+
+         // Initialize CAA blockchain parameters
+         trx.clear();
+         db.modify(global_property_id_type()(db), [](global_property_object &gpo) {
+            gpo.parameters.extensions.value.custom_authority_options = custom_authority_options_type();
+         });
+         set_expiration(db, trx);
+
+
+         //////
+         // Initialize: Accounts
+         //////
+         ACTORS((alice)(bob)(gateway));
+         int64_t init_balance(1000 * GRAPHENE_BLOCKCHAIN_PRECISION );
+         transfer( committee_account, alice_id, graphene::chain::asset(init_balance) );
+         int64_t init_gateway_balance(50 * GRAPHENE_BLOCKCHAIN_PRECISION);
+         transfer( committee_account, gateway_id, graphene::chain::asset(init_gateway_balance) );
+
+
+         //////
+         // Initialize: Pre-image sizes and pre-images to reduce the test variability
+         //////
+         uint16_t pre_image_size_256 = 256;
+         std::vector<char> pre_image_256(pre_image_size_256);
+         generate_random_preimage(pre_image_size_256, pre_image_256);
+
+
+         //////
+         // Gateway puts a contract on the blockchain
+         // This should succeed because the gateway is authorized to create HTLC for its own account
+         //////
+         share_type htlc_amount = 25 * GRAPHENE_BLOCKCHAIN_PRECISION;
+         {
+            graphene::chain::htlc_create_operation create_operation;
+            create_operation.amount = graphene::chain::asset( htlc_amount );
+            create_operation.from = alice_id;
+            create_operation.to = gateway_id;
+            create_operation.claim_period_seconds = 86400;
+            create_operation.preimage_hash = hash_it<fc::ripemd160>( pre_image_256 );
+            create_operation.preimage_size = pre_image_size_256;
+            create_operation.fee = db.current_fee_schedule().calculate_fee( create_operation );
+            trx.clear();
+            trx.operations.push_back(create_operation);
+            sign(trx, alice_private_key);
+            PUSH_TX( db, trx );
+         }
+
+
+         //////
+         // Advance the blockchain to get the finalized HTLC ID
+         //////
+         generate_blocks(1);
+         graphene::chain::htlc_id_type alice_htlc_id =
+                 db.get_index_type<htlc_index>().indices().get<by_from_id>().find(alice.get_id())->id;
+
+
+         //////
+         // Bob attempts to extend the HTLC
+         // This should fail because Bob is not authorized to extend an HTLC on behalf of Alice
+         //////
+         graphene::chain::htlc_extend_operation extend_operation;
+         {
+            extend_operation.update_issuer = alice_id;
+            extend_operation.htlc_id = alice_htlc_id;
+            extend_operation.seconds_to_add = int64_t(24 * 3600);
+            extend_operation.fee = db.current_fee_schedule().calculate_fee( extend_operation );
+            trx.clear();
+            trx.operations.push_back(extend_operation);
+            sign(trx, bob_private_key);
+            BOOST_CHECK_THROW(PUSH_TX(db, trx), tx_missing_active_auth);
+            // The failure should not indicate any rejected custom auths because no CAA applies for Bob's attempt
+            // "rejected_custom_auths":[]
+            EXPECT_EXCEPTION_STRING("\"rejected_custom_auths\":[]", [&] {PUSH_TX(db, trx);});
+
+         }
+
+
+         //////
+         // Alice authorizes Bob to extend an HTLC
+         // by greater than 1 hour and less than or equal to 24 hours
+         //////
+         custom_authority_create_operation authorize_htlc_extension;
+         authorize_htlc_extension.account = alice.get_id();
+         authorize_htlc_extension.auth.add_authority(bob.get_id(), 1);
+         authorize_htlc_extension.auth.weight_threshold = 1;
+         authorize_htlc_extension.enabled = true;
+         authorize_htlc_extension.valid_to = db.head_block_time() + 1000;
+         authorize_htlc_extension.operation_type = operation::tag<htlc_extend_operation>::value;
+
+         // Authorization to extend is restricted to greater than 1 hour and less than or equal to 24 hours
+         vector<restriction> restrictions;
+         auto extension_duration_index = member_index<htlc_extend_operation>("seconds_to_add");
+         // Duration extension greater than one hour
+         restriction restriction_gt_duration = restriction(extension_duration_index, FUNC(gt), int64_t(1 * 3600));
+         restrictions.emplace_back(restriction_gt_duration);
+         // Duration extension less than or equal to 24 hours
+         restriction restriction_le_duration = restriction(extension_duration_index, FUNC(le), int64_t(24 * 3600));
+         restrictions.emplace_back(restriction_le_duration);
+         authorize_htlc_extension.restrictions = restrictions;
+         //[
+         //  {
+         //    "member_index": 3,
+         //    "restriction_type": 4,
+         //    "argument": [
+         //      2,
+         //      3600
+         //    ],
+         //    "extensions": []
+         //  },
+         //  {
+         //    "member_index": 3,
+         //    "restriction_type": 3,
+         //    "argument": [
+         //      2,
+         //      86400
+         //    ],
+         //    "extensions": []
+         //  }
+         //]
+         trx.clear();
+         trx.operations = {authorize_htlc_extension};
+         sign(trx, alice_private_key);
+         PUSH_TX(db, trx);
+
+
+         //////
+         // Advance the blockchain to generate distinctive hash IDs for the similar transactions
+         //////
+         generate_blocks(1);
+
+
+         //////
+         // Bob attempts to extend the HTLC
+         // This should succeed because Bob is conditionally authorized to extend
+         //////
+         {
+            trx.clear();
+            trx.operations.push_back(extend_operation);
+            sign(trx, bob_private_key);
+            PUSH_TX(db, trx);
+
+         }
+
+
+         //////
+         // Bob attempts to extend the HTLC by exactly 10 hours
+         // This should succeed because Bob is authorized to extend the HTLC
+         // if greater than 1 hour and less than or equal to 24 hours
+         //////
+         {
+            extend_operation = htlc_extend_operation();
+            extend_operation.update_issuer = alice_id;
+            extend_operation.htlc_id = alice_htlc_id;
+            extend_operation.seconds_to_add = int64_t(10 * 3600);
+            extend_operation.fee = db.current_fee_schedule().calculate_fee( extend_operation );
+            trx.clear();
+            trx.operations.push_back(extend_operation);
+            sign(trx, bob_private_key);
+            PUSH_TX(db, trx);
+
+         }
+
+
+         //////
+         // Bob attempts to extend the HTLC by exactly 1 hour
+         // This should fail because Bob is authorized to extend the HTLC
+         // if greater than 1 hour and less than or equal to 24 hours
+         //////
+         {
+            extend_operation = htlc_extend_operation();
+            extend_operation.update_issuer = alice_id;
+            extend_operation.htlc_id = alice_htlc_id;
+            extend_operation.seconds_to_add = int64_t(1 * 3600);
+            extend_operation.fee = db.current_fee_schedule().calculate_fee( extend_operation );
+            trx.clear();
+            trx.operations.push_back(extend_operation);
+            sign(trx, bob_private_key);
+            BOOST_CHECK_THROW(PUSH_TX(db, trx), tx_missing_active_auth);
+            // The failure should indicate the rejection path
+            // "rejection_path":[[0,0],[2,"predicate_was_false"]
+            // [0,0]: 0 is the rejection_indicator for an index to a sub-restriction; 0 is the index value for Restriction 1
+            // [2,"predicate_was_false"]: 0 is the rejection_indicator for rejection_reason; "predicate_was_false" is the reason
+            EXPECT_EXCEPTION_STRING("\"rejection_path\":[[0,0],[2,\"predicate_was_false\"]]", [&] {PUSH_TX(db, trx);});
+
+         }
+
+
+         //////
+         // Bob attempts to extend the HTLC by 24 hours plus 1 second
+         // This should fail because Bob is authorized to extend the HTLC
+         // if greater than 1 hour and less than or equal to 24 hours
+         //////
+         {
+            extend_operation = htlc_extend_operation();
+            extend_operation.update_issuer = alice_id;
+            extend_operation.htlc_id = alice_htlc_id;
+            extend_operation.seconds_to_add = int64_t( (24 * 3600) + 1);
+            extend_operation.fee = db.current_fee_schedule().calculate_fee( extend_operation );
+            trx.clear();
+            trx.operations.push_back(extend_operation);
+            sign(trx, bob_private_key);
+            BOOST_CHECK_THROW(PUSH_TX(db, trx), tx_missing_active_auth);
+            // The failure should indicate the rejection path
+            // "rejection_path":[[0,1],[2,"predicate_was_false"]
+            // [0,1]: 0 is the rejection_indicator for an index to a sub-restriction; 1 is the index value for Restriction 2
+            // [2,"predicate_was_false"]: 0 is the rejection_indicator for rejection_reason; "predicate_was_false" is the reason
+            EXPECT_EXCEPTION_STRING("\"rejection_path\":[[0,1],[2,\"predicate_was_false\"]]", [&] {PUSH_TX(db, trx);});
+
+         }
+
+      } FC_LOG_AND_RETHROW()
+   }
+
+
 BOOST_AUTO_TEST_SUITE_END()
