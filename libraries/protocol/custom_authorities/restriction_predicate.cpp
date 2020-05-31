61f4f610827bc38996f4b499d8ddfce4f9e440fd
abitmore <abitmore@users.noreply.github.com>
2020-05-31 00:15:52 +0000

    Split operations to more lists, fix Windows build

--- libraries/protocol/custom_authorities/restriction_predicate.cpp
+++ libraries/protocol/custom_authorities/restriction_predicate.cpp
@@ -52,6 +52,10 @@ restriction_predicate_function get_restriction_predicate(vector<restriction> rs,
          return get_restriction_predicate_list_9(typelist::index_of<operation_list_9::list, Op>(), std::move(rs));
       if (typelist::contains<operation_list_10::list, Op>())
          return get_restriction_predicate_list_10(typelist::index_of<operation_list_10::list, Op>(), std::move(rs));
+      if (typelist::contains<operation_list_11::list, Op>())
+         return get_restriction_predicate_list_11(typelist::index_of<operation_list_11::list, Op>(), std::move(rs));
+      if (typelist::contains<operation_list_12::list, Op>())
+         return get_restriction_predicate_list_12(typelist::index_of<operation_list_12::list, Op>(), std::move(rs));
       if (typelist::contains<virtual_operations_list::list, Op>())
          FC_THROW_EXCEPTION( fc::assert_exception, "Virtual operations not allowed!" );
 
@@ -61,6 +65,7 @@ restriction_predicate_function get_restriction_predicate(vector<restriction> rs,
                                                         operation_list_5::list, operation_list_6::list,
                                                         operation_list_7::list, operation_list_8::list,
                                                         operation_list_9::list, operation_list_10::list,
+                                                        operation_list_11::list, operation_list_12::list,
                                                         virtual_operations_list::list>,
                                        Op>(), "");
       FC_THROW_EXCEPTION(fc::assert_exception,
--- libraries/protocol/custom_authorities/sliced_lists.hxx
+++ libraries/protocol/custom_authorities/sliced_lists.hxx
@@ -32,19 +32,21 @@ namespace typelist = fc::typelist;
 // To make the build gentler on RAM, break the operation list into several pieces to build over several files
 using operation_list_1 = static_variant<typelist::slice<operation::list, 0, 4>>;
 using operation_list_2 = static_variant<typelist::slice<operation::list, 5, 9>>;
-using operation_list_3 = static_variant<typelist::slice<operation::list, 9, 15>>;
-using operation_list_4 = static_variant<typelist::slice<operation::list, 15, 22>>;
-using operation_list_5 = static_variant<typelist::slice<operation::list, 22, 29>>;
-using operation_list_6 = static_variant<typelist::slice<operation::list, 29, 32>>;
-using operation_list_7 = static_variant<typelist::slice<operation::list, 32, 35>>;
-using operation_list_8 = static_variant<typelist::slice<operation::list, 35, 42>>;
-using operation_list_9 = static_variant<typelist::builder<>
+using operation_list_3 = static_variant<typelist::slice<operation::list, 9, 11>>;
+using operation_list_4 = static_variant<typelist::slice<operation::list, 11, 12>>;
+using operation_list_5 = static_variant<typelist::slice<operation::list, 12, 15>>;
+using operation_list_6 = static_variant<typelist::slice<operation::list, 15, 22>>;
+using operation_list_7 = static_variant<typelist::slice<operation::list, 22, 29>>;
+using operation_list_8 = static_variant<typelist::slice<operation::list, 29, 32>>;
+using operation_list_9 = static_variant<typelist::slice<operation::list, 32, 35>>;
+using operation_list_10 = static_variant<typelist::slice<operation::list, 35, 42>>;
+using operation_list_11 = static_variant<typelist::builder<>
                                                 ::add<asset_claim_fees_operation> // 43
                                                 ::add<bid_collateral_operation>   // 45
                                                 ::add_list<typelist::slice<operation::list, 47, 51>>
                                                 ::add<htlc_extend_operation>      // 52
                                                 ::finalize>;
-using operation_list_10 = static_variant<typelist::slice<operation::list, 54>>;
+using operation_list_12 = static_variant<typelist::slice<operation::list, 54>>;
 using virtual_operations_list = static_variant<fill_order_operation,          // 4
                                                asset_settle_cancel_operation, // 42
                                                fba_distribute_operation,      // 44
@@ -63,5 +65,7 @@ object_restriction_predicate<operation> get_restriction_predicate_list_7(size_t 
 object_restriction_predicate<operation> get_restriction_predicate_list_8(size_t idx, vector<restriction> rs);
 object_restriction_predicate<operation> get_restriction_predicate_list_9(size_t idx, vector<restriction> rs);
 object_restriction_predicate<operation> get_restriction_predicate_list_10(size_t idx, vector<restriction> rs);
+object_restriction_predicate<operation> get_restriction_predicate_list_11(size_t idx, vector<restriction> rs);
+object_restriction_predicate<operation> get_restriction_predicate_list_12(size_t idx, vector<restriction> rs);
 
 } } // namespace graphene::protocol
