189fa772310e07d3c093f85f0f2178d26a17e528
abitmore <abitmore@users.noreply.github.com>
2021-03-04 18:39:15 +0000

    Force TLSv1.2 or above when using libcurl

--- libraries/plugins/elasticsearch/elasticsearch_plugin.cpp
+++ libraries/plugins/elasticsearch/elasticsearch_plugin.cpp
@@ -38,7 +38,10 @@ class elasticsearch_plugin_impl
    public:
       elasticsearch_plugin_impl(elasticsearch_plugin& _plugin)
          : _self( _plugin )
-      {  curl = curl_easy_init(); }
+      {
+         curl = curl_easy_init();
+         curl_easy_setopt(curl, CURLOPT_SSLVERSION, CURL_SSLVERSION_TLSv1_2);
+      }
       virtual ~elasticsearch_plugin_impl();
 
       bool update_account_histories( const signed_block& b );
