667a18d93d23a0f629f7c3ef66b3bdf3c69a5bf2
abitmore <abitmore@users.noreply.github.com>
2021-03-09 10:38:25 -0500

    Add pragma once to hpp files in tests/common

--- tests/common/utils.hpp
+++ tests/common/utils.hpp
@@ -21,6 +21,7 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
+#pragma once
 
 #include <fc/thread/thread.hpp>
 #include <boost/test/included/unit_test.hpp>
