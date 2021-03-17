b323f27d329476bd6170c07af1b97c004fb16605
Abit <abitmore@users.noreply.github.com>
2021-03-17 23:17:35 +0100

    Update comment

--- libraries/chain/liquidity_pool_evaluator.cpp
+++ libraries/chain/liquidity_pool_evaluator.cpp
@@ -375,8 +375,9 @@ generic_exchange_operation_result liquidity_pool_exchange_evaluator::do_apply(
    d.adjust_balance( op.account, _account_receives );
 
    // For _pool_receives_asset, if market fee sharing is enabled,
-   // the share asset owner's registrar and referrer get the shared maker market fee.
-   // For _pool_pays_asset, it's the trader's registrar and referrer.
+   // the share asset owner's registrar and referrer will get the shared maker market fee.
+   // For _pool_pays_asset, if market fee sharing is enabled,
+   // the trader's registrar and referrer will get the shared taker market fee.
    d.pay_market_fees( &_pool->share_asset(d).issuer(d), *_pool_receives_asset, op.amount_to_sell, true,
                       _maker_market_fee );
    d.pay_market_fees( fee_paying_account, *_pool_pays_asset, _pool_pays, false, _taker_market_fee );
