0f3571ced1afc56408bba0279a36a7d2e0665b30
Abit <abitmore@users.noreply.github.com>
2021-03-21 18:19:43 +0100

    Remove unused variables

--- libraries/wallet/wallet_sign.cpp
+++ libraries/wallet/wallet_sign.cpp
@@ -138,11 +138,11 @@ namespace graphene { namespace wallet { namespace detail {
       try {
          account_object from_account = get_account(from);
          md.from = from_account.options.memo_key;
-      } catch (const fc::exception& e) {
+      } catch (const fc::exception&) {
          // check if the string itself is a pubkey, if not, consider it as a label
          try {
             md.from = public_key_type( from );
-         } catch (const fc::exception& e) {
+         } catch (const fc::exception&) {
             md.from = self.get_public_key( from );
          }
       }
@@ -150,11 +150,11 @@ namespace graphene { namespace wallet { namespace detail {
       try {
          account_object to_account = get_account(to);
          md.to = to_account.options.memo_key;
-      } catch (const fc::exception& e) {
+      } catch (const fc::exception&) {
          // check if the string itself is a pubkey, if not, consider it as a label
          try {
             md.to = public_key_type( to );
-         } catch (const fc::exception& e) {
+         } catch (const fc::exception&) {
             md.to = self.get_public_key( to );
          }
       }
@@ -162,7 +162,7 @@ namespace graphene { namespace wallet { namespace detail {
       // try to get private key of from and sign, if that fails, try to sign with to
       try {
          md.set_message(get_private_key(md.from), md.to, memo);
-      } catch (const fc::exception& e) {
+      } catch (const fc::exception&) {
          std::swap( md.from, md.to );
          md.set_message(get_private_key(md.from), md.to, memo);
          std::swap( md.from, md.to );
