564de55a07cbed0fa6987b826a0bc6536df989d3
abitmore <abitmore@users.noreply.github.com>
2021-07-06 14:24:19 +0000

    Fix code smells

--- libraries/protocol/include/graphene/protocol/base.hpp
+++ libraries/protocol/include/graphene/protocol/base.hpp
@@ -128,10 +128,10 @@ namespace graphene { namespace protocol {
       {
          return params.fee;
       }
-      virtual void get_required_authorities( vector<authority>& )const{}
-      virtual void get_required_active_authorities( flat_set<account_id_type>& )const{}
-      virtual void get_required_owner_authorities( flat_set<account_id_type>& )const{}
-      virtual void validate()const{}
+      virtual void get_required_authorities( vector<authority>& )const{ /* do nothing by default */ }
+      virtual void get_required_active_authorities( flat_set<account_id_type>& )const{ /* do nothing by default */ }
+      virtual void get_required_owner_authorities( flat_set<account_id_type>& )const{ /* do nothing by default */ }
+      virtual void validate()const{ /* do nothing by default */ }
       fc::optional< fc::future<void> > validate_parallel( uint32_t skip )const;
 
       static uint64_t calculate_data_fee( uint64_t bytes, uint64_t price_per_kbyte );
