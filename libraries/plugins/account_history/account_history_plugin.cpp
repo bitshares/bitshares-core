598553404d387bfe7f9ea5cfd8aeb0109484eada
abitmore <abitmore@users.noreply.github.com>
2022-09-30 15:57:17 +0000

    Add option to keep operations in the last X blocks

--- libraries/plugins/account_history/account_history_plugin.cpp
+++ libraries/plugins/account_history/account_history_plugin.cpp
@@ -35,6 +35,8 @@
 #include <graphene/chain/transaction_evaluation_state.hpp>
 #include <graphene/chain/hardfork.hpp>
 
+#include <graphene/utilities/boost_program_options.hpp>
+
 #include <fc/thread/thread.hpp>
 
 namespace graphene { namespace account_history {
@@ -50,6 +52,7 @@ class account_history_plugin_impl
          : _self( _plugin )
       { }
 
+   private:
       /** this method is called as a callback after a block is applied
        * and will process/index all operations that were applied in the block.
        */
@@ -62,7 +65,6 @@ class account_history_plugin_impl
 
       friend class graphene::account_history::account_history_plugin;
 
-   private:
       account_history_plugin& _self;
       flat_set<account_id_type> _tracked_accounts;
       flat_set<account_id_type> _extended_history_accounts;
@@ -71,10 +73,19 @@ class account_history_plugin_impl
       primary_index< operation_history_index >* _oho_index;
       uint64_t _max_ops_per_account = -1;
       uint64_t _extended_max_ops_per_account = -1;
+      uint32_t _min_blocks_to_keep = 30000;
+      uint64_t _max_ops_per_acc_by_min_blocks = 1000;
 
-      /** add one history record, then check and remove the earliest history record */
-      void add_account_history( const account_id_type account_id, const operation_history_id_type op_id );
+      uint64_t get_max_ops_to_keep( const account_id_type& account_id );
 
+      /** add one history record, then check and remove the earliest history record(s) */
+      void add_account_history( const account_id_type& account_id, const operation_history_object& op );
+
+      void remove_old_histories( const account_statistics_object& stats_obj,
+                                 const account_history_object& latest_aho,
+                                 const operation_history_object& latest_op );
+
+      void init_program_options(const boost::program_options::variables_map& options);
 };
 
 void account_history_plugin_impl::update_account_histories( const signed_block& b )
@@ -182,7 +193,7 @@ void account_history_plugin_impl::update_account_histories( const signed_block& 
                // that indexing now happens in observers' post_evaluate()
 
                // add history
-               add_account_history( account_id, oho->id );
+               add_account_history( account_id, *oho );
             }
          }
       }
@@ -202,7 +213,7 @@ void account_history_plugin_impl::update_account_histories( const signed_block& 
                {
                   if (!oho.valid()) { oho = create_oho(); }
                   // add history
-                  add_account_history( account_id, oho->id );
+                  add_account_history( account_id, *oho );
                }
             }
          }
@@ -212,72 +223,100 @@ void account_history_plugin_impl::update_account_histories( const signed_block& 
    }
 }
 
-void account_history_plugin_impl::add_account_history( const account_id_type account_id,
-                                                       const operation_history_id_type op_id )
+void account_history_plugin_impl::add_account_history( const account_id_type& account_id,
+                                                       const operation_history_object& op )
 {
    graphene::chain::database& db = database();
-   const auto& stats_obj = account_id(db).statistics(db);
+   const auto& stats_obj = db.get_account_stats_by_owner( account_id );
    // add new entry
-   const auto& ath = db.create<account_history_object>( [&]( account_history_object& obj ){
-       obj.operation_id = op_id;
+   const auto& aho = db.create<account_history_object>( [&account_id,&op,&stats_obj](account_history_object& obj){
+       obj.operation_id = op.id;
        obj.account = account_id;
        obj.sequence = stats_obj.total_ops + 1;
        obj.next = stats_obj.most_recent_op;
    });
-   db.modify( stats_obj, [&]( account_statistics_object& obj ){
-       obj.most_recent_op = ath.id;
-       obj.total_ops = ath.sequence;
+   db.modify( stats_obj, [&aho]( account_statistics_object& obj ){
+       obj.most_recent_op = aho.id;
+       obj.total_ops = aho.sequence;
    });
+   // Remove the earliest account history entries if too many.
+   remove_old_histories( stats_obj, aho, op );
+}
+
+uint64_t account_history_plugin_impl::get_max_ops_to_keep( const account_id_type& account_id )
+{
+   const graphene::chain::database& db = database();
    // Amount of history to keep depends on if account is in the "extended history" list
    bool extended_hist = ( _extended_history_accounts.find( account_id ) != _extended_history_accounts.end() );
-   if( !extended_hist && !_extended_history_registrars.empty() ) {
-      const account_id_type registrar_id = account_id(db).registrar;
+   if( !extended_hist && !_extended_history_registrars.empty() )
+   {
+      const account_id_type& registrar_id = account_id(db).registrar;
       extended_hist = ( _extended_history_registrars.find( registrar_id ) != _extended_history_registrars.end() );
    }
    // _max_ops_per_account is guaranteed to be non-zero outside; max_ops_to_keep
    // will likewise be non-zero, and also non-negative (it is unsigned).
    auto max_ops_to_keep = _max_ops_per_account;
-   if (extended_hist && _extended_max_ops_per_account > max_ops_to_keep) {
+   if( extended_hist && _extended_max_ops_per_account > max_ops_to_keep )
+   {
       max_ops_to_keep = _extended_max_ops_per_account;
    }
-   // Remove the earliest account history entry if too many.
-   if( stats_obj.total_ops - stats_obj.removed_ops > max_ops_to_keep )
+   if( 0 == max_ops_to_keep )
+      return 1;
+   return max_ops_to_keep;
+}
+
+// Remove the earliest account history entries if too many.
+void account_history_plugin_impl::remove_old_histories( const account_statistics_object& stats_obj,
+                                                        const account_history_object& latest_aho,
+                                                        const operation_history_object& latest_op )
+{
+   graphene::chain::database& db = database();
+   const account_id_type& account_id = latest_aho.account;
+   auto max_ops_to_keep = get_max_ops_to_keep( account_id );
+
+   while( stats_obj.total_ops - stats_obj.removed_ops > max_ops_to_keep )
    {
       // look for the earliest entry
       const auto& his_idx = db.get_index_type<account_history_index>();
       const auto& by_seq_idx = his_idx.indices().get<by_seq>();
-      auto itr = by_seq_idx.lower_bound( boost::make_tuple( account_id, 0 ) );
+      auto itr = by_seq_idx.lower_bound( account_id );
       // make sure don't remove the one just added
-      if( itr != by_seq_idx.end() && itr->account == account_id && itr->id != ath.id )
-      {
-         // if found, remove the entry, and adjust account stats object
-         const auto remove_op_id = itr->operation_id;
-         const auto itr_remove = itr;
-         ++itr;
-         db.remove( *itr_remove );
-         db.modify( stats_obj, [&]( account_statistics_object& obj ){
-             obj.removed_ops = obj.removed_ops + 1;
-         });
-         // modify previous node's next pointer
-         // this should be always true, but just have a check here
-         if( itr != by_seq_idx.end() && itr->account == account_id )
-         {
-            db.modify( *itr, [&]( account_history_object& obj ){
-               obj.next = account_history_id_type();
-            });
-         }
-         // else need to modify the head pointer, but it shouldn't be true
+      if( itr == by_seq_idx.end() || itr->account != account_id || itr->id == latest_aho.id )
+         return;
 
-         // remove the operation history entry (1.11.x) if configured and no reference left
-         if( _partial_operations )
+      // if found, check whether to remove
+      const auto remove_op_id = itr->operation_id;
+      const auto& remove_op = remove_op_id(db);
+      if( remove_op.block_num + _min_blocks_to_keep > latest_op.block_num
+          && stats_obj.total_ops - stats_obj.removed_ops <= _max_ops_per_acc_by_min_blocks )
+         return;
+
+      // remove the entry, and adjust account stats object
+      const auto itr_remove = itr;
+      ++itr;
+      db.remove( *itr_remove );
+      db.modify( stats_obj, [&]( account_statistics_object& obj ){
+          obj.removed_ops = obj.removed_ops + 1;
+      });
+      // modify previous node's next pointer
+      // this should be always true, but just have a check here
+      if( itr != by_seq_idx.end() && itr->account == account_id )
+      {
+         db.modify( *itr, [&]( account_history_object& obj ){
+            obj.next = account_history_id_type();
+         });
+      }
+      // else need to modify the head pointer, but it shouldn't be true
+
+      // remove the operation history entry (1.11.x) if configured and no reference left
+      if( _partial_operations )
+      {
+         // check for references
+         const auto& by_opid_idx = his_idx.indices().get<by_opid>();
+         if( by_opid_idx.find( remove_op_id ) == by_opid_idx.end() )
          {
-            // check for references
-            const auto& by_opid_idx = his_idx.indices().get<by_opid>();
-            if( by_opid_idx.find( remove_op_id ) == by_opid_idx.end() )
-            {
-               // if no reference, remove
-               db.remove( remove_op_id(db) );
-            }
+            // if no reference, remove
+            db.remove( remove_op );
          }
       }
    }
@@ -315,7 +354,8 @@ void account_history_plugin::plugin_set_program_options(
          ("partial-operations", boost::program_options::value<bool>(),
           "Keep only those operations in memory that are related to account history tracking")
          ("max-ops-per-account", boost::program_options::value<uint64_t>(),
-          "Maximum number of operations per account that will be kept in memory")
+          "Maximum number of operations per account that will be kept in memory. "
+          "Note that the actual number may be higher due to the min-blocks-to-keep option.")
          ("extended-max-ops-per-account", boost::program_options::value<uint64_t>(),
           "Maximum number of operations to keep for accounts for which extended history is kept")
          ("extended-history-by-account",
@@ -324,31 +364,46 @@ void account_history_plugin::plugin_set_program_options(
          ("extended-history-by-registrar",
           boost::program_options::value<std::vector<std::string>>()->composing()->multitoken(),
           "Track longer history for accounts with this registrar (may specify multiple times)")
+         ("min-blocks-to-keep", boost::program_options::value<uint32_t>(),
+          "Operations which are in the latest X blocks will be kept in memory. "
+          "Note that this may exceed the limit defined by the max-ops-per-account option, "
+          "but will be limited by the max-ops-per-acc-by-min-blocks option.")
+         ("max-ops-per-acc-by-min-blocks", boost::program_options::value<uint64_t>(),
+          "A potential higher limit on the maximum number of operations per account to be kept in memory "
+          "when the min-blocks-to-keep option exceeds the limit defined by the max-ops-per-account option. "
+          "If this is less than max-ops-per-account, max-ops-per-account will be used.")
          ;
    cfg.add(cli);
 }
 
 void account_history_plugin::plugin_initialize(const boost::program_options::variables_map& options)
 {
+   my->init_program_options( options );
+
    database().applied_block.connect( [&]( const signed_block& b){ my->update_account_histories(b); } );
    my->_oho_index = database().add_index< primary_index< operation_history_index > >();
    database().add_index< primary_index< account_history_index > >();
+}
 
-   LOAD_VALUE_SET(options, "track-account", my->_tracked_accounts, graphene::chain::account_id_type);
-   if (options.count("partial-operations") > 0) {
-       my->_partial_operations = options["partial-operations"].as<bool>();
-   }
-   if (options.count("max-ops-per-account") > 0) {
-       my->_max_ops_per_account = options["max-ops-per-account"].as<uint64_t>();
-   }
-   if (options.count("extended-max-ops-per-account") > 0) {
-       auto emopa = options["extended-max-ops-per-account"].as<uint64_t>();
-       my->_extended_max_ops_per_account = (emopa > my->_max_ops_per_account) ? emopa : my->_max_ops_per_account;
-   }
-   LOAD_VALUE_SET(options, "extended-history-by-account", my->_extended_history_accounts,
+void detail::account_history_plugin_impl::init_program_options(const boost::program_options::variables_map& options)
+{
+   LOAD_VALUE_SET(options, "track-account", _tracked_accounts, graphene::chain::account_id_type);
+
+   utilities::get_program_option( options, "partial-operations", _partial_operations );
+   utilities::get_program_option( options, "max-ops-per-account", _max_ops_per_account );
+   utilities::get_program_option( options, "extended-max-ops-per-account", _extended_max_ops_per_account );
+   if( _extended_max_ops_per_account < _max_ops_per_account )
+      _extended_max_ops_per_account = _max_ops_per_account;
+
+   LOAD_VALUE_SET(options, "extended-history-by-account", _extended_history_accounts,
                   graphene::chain::account_id_type);
-   LOAD_VALUE_SET(options, "extended-history-by-registrar", my->_extended_history_registrars,
+   LOAD_VALUE_SET(options, "extended-history-by-registrar", _extended_history_registrars,
                   graphene::chain::account_id_type);
+
+   utilities::get_program_option( options, "min-blocks-to-keep", _min_blocks_to_keep );
+   utilities::get_program_option( options, "max-ops-per-acc-by-min-blocks", _max_ops_per_acc_by_min_blocks );
+   if( _max_ops_per_acc_by_min_blocks < _max_ops_per_account )
+      _max_ops_per_acc_by_min_blocks = _max_ops_per_account;
 }
 
 void account_history_plugin::plugin_startup()
